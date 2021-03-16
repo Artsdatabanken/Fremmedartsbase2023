9bc49ab0e83e157df3e1c539cfc30812de02689b
Stein Arild Hoem <stein.hoem@artsdatabanken.no>
2021-03-16 14:14:16 +0100

    Kodelister som ressurs

--- Prod.Api/Controllers/KodeController.cs
+++ Prod.Api/Controllers/KodeController.cs
@@ -1,4 +1,7 @@
-﻿using System.Linq;
+﻿using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Cors;
 using Prod.Api.Services;
@@ -18,13 +21,18 @@ namespace Prod.Api.Controllers
         }
 
         [HttpGet("{id}")]
-        public JsonResult Get(int id)
+        public async System.Threading.Tasks.Task<string> GetAsync(int id)
         {
-
-            var datastring = _dbContext.Codes.First(x => x.Id == id).JsonData;
-            //return !string.IsNullOrWhiteSpace(data) ? Json(data) : null;
-            var data = Newtonsoft.Json.Linq.JObject.Parse(datastring);
-            return Json(data);
+            var assembly = Assembly.GetEntryAssembly();
+            var resourceStream = assembly.GetManifestResourceStream("Prod.Api.Resources.Codes.json");
+            using (var reader = new StreamReader(resourceStream, Encoding.UTF8))
+            {
+                return await reader.ReadToEndAsync();
+            }
+            //var datastring = _dbContext.Codes.First(x => x.Id == id).JsonData;
+            ////return !string.IsNullOrWhiteSpace(data) ? Json(data) : null;
+            //var data = Newtonsoft.Json.Linq.JObject.Parse(datastring);
+            //return Json(data);
         }
 
     }
