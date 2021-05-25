e506cf60c04f37e1299afac9cb34f4fe2fbd5879
Stein Arild Hoem <stein.hoem@artsdatabanken.no>
2021-05-25 13:37:23 +0200

    config test

--- Prod.Web/src/config.js
+++ Prod.Web/src/config.js
@@ -4,11 +4,11 @@ function apiUrl (anchor) {
       case '#lokalapi':
         return 'http://localhost:25808'
       case '#remoteapi':
-          return 'https://rl2021api.test.artsdatabanken.no'
+          return 'https://fab4api.test.artsdatabanken.no/'
       default:
         //return relPath   // This does not currently work because api is not relative to ("under") app 
-        if (window.location.href.indexOf("test.") > 1) {return 'https://rl2021api.test.artsdatabanken.no'}
-        return  process.env.NODE_ENV === 'development' ? 'http://localhost:25808' : 'https://rl2021api.artsdatabanken.no'
+        if (window.location.href.indexOf("test.") > 1) {return 'https://fab4api.test.artsdatabanken.no'}
+        return  process.env.NODE_ENV === 'development' ? 'http://localhost:25808' : 'https://fab4api.test.artsdatabanken.no'
     }
   }
   function referenceApiUrl (anchor) {
