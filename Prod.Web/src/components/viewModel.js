e93304feb871027cdb470d9456784bb275c5430b
bugdriven <helgesandmark@hotmail.com>
2021-06-05 19:58:43 +0200

    viewModel reorg 6

--- Prod.Web/src/components/viewModel.js
+++ Prod.Web/src/components/viewModel.js
@@ -27,6 +27,117 @@ class tabItem {
 }
 
 
+function tabdefs(appState) {
+    return {
+    selectAssessmentTabs: {
+        activeTab: {id: 1},
+        tabList: () => [
+            new tabItem({id: 1, label:appState.koder.mainSelectAssessment, enabled:true}),
+            new tabItem({id: 2, label:appState.koder.mainCreateAssessment, enabled:true}),
+            new tabItem({id: 3, label:appState.koder.mainStatistics, enabled:true, visible: appState.showstatistikk}),
+            new tabItem({id: 4, label:appState.koder.mainReport, enabled:true})
+        ],
+        setActiveTab: (tab) => {
+            action(() => {
+                console.log('xxx', appState.selectAssessmentTabs.activeTab)
+                console.log('xxx', tab)
+                if(tab.enabled) {
+                    appState.selectAssessmentTabs.activeTab.id = tab.id
+                    appState.assessmentId = null
+                    //                            appState.router.hash = "#" + tab.url + "/" + appState.assessmentId
+                }
+            })()
+        }
+    },
+
+
+    assessmentTabs: {
+        activeTab: {id: 0},
+        tabList: [
+            new tabItem({id: 0, label:"Horisontskanning", enabled: appState.harVurdering, url: "horisontskanning" }),
+            new tabItem({id: 1, label:"Artens status", enabled: appState.harVurdering, url: "artensstatus" }),
+            new tabItem({id: 2, label:"Artsinformasjon", enabled: appState.harVurdering, notrequired: !appState.skalVurderes, url: "artsegenskaper" }),
+            new tabItem({id: 3, label:"Spredningsveier", enabled: appState.harVurdering, notrequired: !appState.skalVurderes, url: "import" }),
+            //new tabItem({id: 4, label:"Spredningsveier", enabled: appState.harVurdering, notrequired: !appState.skalVurderes, url: "spredningsveier" }),
+            new tabItem({id: 4, label:"Informasjon for risikovurdering", enabled: appState.harVurdering, notrequired: !appState.skalVurderes, url: "utbredelseshistorikk" }),
+            //new tabItem({id: 5, label:"Naturtyper", enabled: appState.harVurdering, notrequired: !appState.skalVurderes, url: "naturtyper" }),
+            new tabItem({id: 5, label:"Risikovurdering", enabled: appState.harVurdering, notrequired: !appState.skalVurderes, url: "invasjonspotensiale" }),
+            //new tabItem({id: 7, label:"Økologisk effekt", enabled: appState.harVurdering, notrequired: !appState.skalVurderes, url: "ekologiskeffekt" }),
+            //new tabItem({id: 9, label:"Geografisk variasjon", enabled: appState.harVurdering, notrequired: !appState.skalVurderes, url: "geografiskvariasjon" }),
+            new tabItem({id: 6, label:"Klimaeffekter", enabled: appState.harVurdering, notrequired: !appState.skalVurderes, url: "klimaeffekter" }),
+            new tabItem({id: 7, label:"Geografisk variasjon", enabled: appState.harVurdering, notrequired: !appState.skalVurderes, url: "geografiskvariasjon" }),
+            //new tabItem({id: 9, label:"Kriteriedokumentasjon", enabled: appState.harVurdering, notrequired: !appState.skalVurderes, url: "kriteriedokumentasjon" }),
+            // new tabItem({id: 9, label:"Oppsummering", enabled: appState.harVurdering, notrequired: !appState.skalVurderes, url: "oppsummering" }),
+            new tabItem({id: 8, label:"Oppsummering", enabled:appState.harVurdering, url: "oppsummmering"}),
+            new tabItem({id: 9, label:"Referanser", enabled:appState.harVurdering, url: "referanser"}),
+            new tabItem({id: 10, label:"Kommentar på vurdering", enabled:appState.harVurdering, url: "kommentar"}),
+            // new tabItem({id: 11, label:"Kommentarer", enabled:appState.harVurdering, url: "Kommentarer"}),
+            new tabItem({id: 11, label:"JSON", enabled:appState.harVurdering, url: "diff"})
+            // new tabItem({id: 1, label:"Artsinformasjon", enabled: appState.harVurdering, url: "artsinformasjon" }),
+            // new tabItem({id: 2, label:"Naturtyper", enabled: appState.harVurdering, notrequired: !appState.skalVurderes, url: "naturtyper" }),
+            // new tabItem({id: 3, label:"Påvirkning", enabled: appState.harVurdering, notrequired: !appState.skalVurderes, url: "pavirkning" }),
+            // new tabItem({id: 4, label:"A", enabled: appState.harVurdering, notrequired: !appState.skalVurderes, url: "a" }),
+            // new tabItem({id: 5, label:"B", enabled: appState.harVurdering, notrequired: !appState.skalVurderes, url: "b" }),
+            // new tabItem({id: 6, label:"C", enabled: appState.harVurdering, notrequired: !appState.skalVurderes, url: "c" }),
+            // new tabItem({id: 7, label:"D", enabled: appState.harVurdering, notrequired: !appState.skalVurderes, url: "d" }),
+            // new tabItem({id: 8, label:"E", enabled: appState.harVurdering, notrequired: !appState.skalVurderes, url: "e" }),
+            // new tabItem({id: 9, label:"Oppsummering", enabled: appState.harVurdering, notrequired: !appState.skalVurderes, url: "oppsummering" }),
+            // new tabItem({id: 10, label:"Referanser", enabled:appState.harVurdering, url: "referanser"}),
+            // new tabItem({id: 11, label:"Kommentarer", enabled:appState.harVurdering, url: "Kommentarer"}),
+            // new tabItem({id: 12, label:"JSON", enabled:appState.harVurdering, url: "diff"})
+        ],
+        setActiveTab: (tab) => {
+            action(() => {
+                if(tab.enabled) {
+                    appState.assessmentTabs.activeTab.id = tab.id
+                    //                            appState.router.hash = "#" + tab.url + "/" + appState.assessmentId
+                }
+            })()
+        }
+    },
+    infoTabs: {
+        activeTab: {id: 1},
+        tabList: [
+            new tabItem({id: 1, label: "Utbredelse", enabled:true}),
+            new tabItem({id: 2, label: "Naturtyper", enabled:true})
+        ],
+        setActiveTab: (tab) => {
+            action(() => {
+                //console.log('xxx', appState.selectAssessmentTabs.activeTab)
+                //console.log('xxx', tab)
+                if(tab.enabled) {
+                    appState.infoTabs.activeTab.id = tab.id
+                    //appState.assessmentId = null
+                    //                            appState.router.hash = "#" + tab.url + "/" + appState.assessmentId
+                }
+            })()
+        }
+    },
+    riskAssessmentTabs: {
+        activeTab: {id: 1},
+        tabList: [
+            new tabItem({id: 1, label:"Invasjonspotensiale", enabled:true}),
+            new tabItem({id: 2, label: "Økologisk effekt", enabled:true})
+        ],
+        setActiveTab: (tab) => {
+            action(() => {
+                //console.log('xxx', appState.riskAssessmentTabs.activeTab)
+                //console.log('xxx', tab)
+                if(tab.enabled) {
+                    appState.riskAssessmentTabs.activeTab.id = tab.id
+                    //appState.assessmentId = null
+                    //                            appState.router.hash = "#" + tab.url + "/" + appState.assessmentId
+                }
+            })()
+        }
+    }
+}}
+
+
+
+
+
+
 class ViewModel {
     constructor() {
         const options = {
@@ -354,110 +465,7 @@ class ViewModel {
         // ***************************************
 
         // **** initialize tabs ****
-        extendObservable(this, {
-            selectAssessmentTabs: {
-                activeTab: {id: 1},
-                tabList: () => [
-                    new tabItem({id: 1, label:this.koder.mainSelectAssessment, enabled:true}),
-                    new tabItem({id: 2, label:this.koder.mainCreateAssessment, enabled:true}),
-                    new tabItem({id: 3, label:this.koder.mainStatistics, enabled:true, visible: this.showstatistikk}),
-                    new tabItem({id: 4, label:this.koder.mainReport, enabled:true})
-                ],
-                setActiveTab: (tab) => {
-                    action(() => {
-                        console.log('xxx', this.selectAssessmentTabs.activeTab)
-                        console.log('xxx', tab)
-                        if(tab.enabled) {
-                            this.selectAssessmentTabs.activeTab.id = tab.id
-                            this.assessmentId = null
-                            //                            this.router.hash = "#" + tab.url + "/" + this.assessmentId
-                        }
-                    })()
-                }
-            },
-
-
-            assessmentTabs: {
-                activeTab: {id: 0},
-                tabList: [
-                    new tabItem({id: 0, label:"Horisontskanning", enabled: this.harVurdering, url: "horisontskanning" }),
-                    new tabItem({id: 1, label:"Artens status", enabled: this.harVurdering, url: "artensstatus" }),
-                    new tabItem({id: 2, label:"Artsinformasjon", enabled: this.harVurdering, notrequired: !this.skalVurderes, url: "artsegenskaper" }),
-                    new tabItem({id: 3, label:"Spredningsveier", enabled: this.harVurdering, notrequired: !this.skalVurderes, url: "import" }),
-                    //new tabItem({id: 4, label:"Spredningsveier", enabled: this.harVurdering, notrequired: !this.skalVurderes, url: "spredningsveier" }),
-                    new tabItem({id: 4, label:"Informasjon for risikovurdering", enabled: this.harVurdering, notrequired: !this.skalVurderes, url: "utbredelseshistorikk" }),
-                    //new tabItem({id: 5, label:"Naturtyper", enabled: this.harVurdering, notrequired: !this.skalVurderes, url: "naturtyper" }),
-                    new tabItem({id: 5, label:"Risikovurdering", enabled: this.harVurdering, notrequired: !this.skalVurderes, url: "invasjonspotensiale" }),
-                    //new tabItem({id: 7, label:"Økologisk effekt", enabled: this.harVurdering, notrequired: !this.skalVurderes, url: "ekologiskeffekt" }),
-                    //new tabItem({id: 9, label:"Geografisk variasjon", enabled: this.harVurdering, notrequired: !this.skalVurderes, url: "geografiskvariasjon" }),
-                    new tabItem({id: 6, label:"Klimaeffekter", enabled: this.harVurdering, notrequired: !this.skalVurderes, url: "klimaeffekter" }),
-                    new tabItem({id: 7, label:"Geografisk variasjon", enabled: this.harVurdering, notrequired: !this.skalVurderes, url: "geografiskvariasjon" }),
-                    //new tabItem({id: 9, label:"Kriteriedokumentasjon", enabled: this.harVurdering, notrequired: !this.skalVurderes, url: "kriteriedokumentasjon" }),
-                    // new tabItem({id: 9, label:"Oppsummering", enabled: this.harVurdering, notrequired: !this.skalVurderes, url: "oppsummering" }),
-                    new tabItem({id: 8, label:"Oppsummering", enabled:this.harVurdering, url: "oppsummmering"}),
-                    new tabItem({id: 9, label:"Referanser", enabled:this.harVurdering, url: "referanser"}),
-                    new tabItem({id: 10, label:"Kommentar på vurdering", enabled:this.harVurdering, url: "kommentar"}),
-                    // new tabItem({id: 11, label:"Kommentarer", enabled:this.harVurdering, url: "Kommentarer"}),
-                    new tabItem({id: 11, label:"JSON", enabled:this.harVurdering, url: "diff"})
-                    // new tabItem({id: 1, label:"Artsinformasjon", enabled: this.harVurdering, url: "artsinformasjon" }),
-                    // new tabItem({id: 2, label:"Naturtyper", enabled: this.harVurdering, notrequired: !this.skalVurderes, url: "naturtyper" }),
-                    // new tabItem({id: 3, label:"Påvirkning", enabled: this.harVurdering, notrequired: !this.skalVurderes, url: "pavirkning" }),
-                    // new tabItem({id: 4, label:"A", enabled: this.harVurdering, notrequired: !this.skalVurderes, url: "a" }),
-                    // new tabItem({id: 5, label:"B", enabled: this.harVurdering, notrequired: !this.skalVurderes, url: "b" }),
-                    // new tabItem({id: 6, label:"C", enabled: this.harVurdering, notrequired: !this.skalVurderes, url: "c" }),
-                    // new tabItem({id: 7, label:"D", enabled: this.harVurdering, notrequired: !this.skalVurderes, url: "d" }),
-                    // new tabItem({id: 8, label:"E", enabled: this.harVurdering, notrequired: !this.skalVurderes, url: "e" }),
-                    // new tabItem({id: 9, label:"Oppsummering", enabled: this.harVurdering, notrequired: !this.skalVurderes, url: "oppsummering" }),
-                    // new tabItem({id: 10, label:"Referanser", enabled:this.harVurdering, url: "referanser"}),
-                    // new tabItem({id: 11, label:"Kommentarer", enabled:this.harVurdering, url: "Kommentarer"}),
-                    // new tabItem({id: 12, label:"JSON", enabled:this.harVurdering, url: "diff"})
-                ],
-                setActiveTab: (tab) => {
-                    action(() => {
-                        if(tab.enabled) {
-                            this.assessmentTabs.activeTab.id = tab.id
-                            //                            this.router.hash = "#" + tab.url + "/" + this.assessmentId
-                        }
-                    })()
-                }
-            },
-            infoTabs: {
-                activeTab: {id: 1},
-                tabList: [
-                    new tabItem({id: 1, label: "Utbredelse", enabled:true}),
-                    new tabItem({id: 2, label: "Naturtyper", enabled:true})
-                ],
-                setActiveTab: (tab) => {
-                    action(() => {
-                        //console.log('xxx', this.selectAssessmentTabs.activeTab)
-                        //console.log('xxx', tab)
-                        if(tab.enabled) {
-                            this.infoTabs.activeTab.id = tab.id
-                            //this.assessmentId = null
-                            //                            this.router.hash = "#" + tab.url + "/" + this.assessmentId
-                        }
-                    })()
-                }
-            },
-            riskAssessmentTabs: {
-                activeTab: {id: 1},
-                tabList: [
-                    new tabItem({id: 1, label:"Invasjonspotensiale", enabled:true}),
-                    new tabItem({id: 2, label: "Økologisk effekt", enabled:true})
-                ],
-                setActiveTab: (tab) => {
-                    action(() => {
-                        //console.log('xxx', this.riskAssessmentTabs.activeTab)
-                        //console.log('xxx', tab)
-                        if(tab.enabled) {
-                            this.riskAssessmentTabs.activeTab.id = tab.id
-                            //this.assessmentId = null
-                            //                            this.router.hash = "#" + tab.url + "/" + this.assessmentId
-                        }
-                    })()
-                }
-            }
-        })
+        extendObservable(this, tabdefs(this))
         // **** end initialize tabs ****
 
 
