c0db0a4b2bac9524499ec431057659f85ad284e9
bugdriven <helgesandmark@hotmail.com>
2021-06-24 19:30:07 +0200

    tabs minor

--- Prod.Web/src/components/tabs.js
+++ Prod.Web/src/components/tabs.js
@@ -9,13 +9,17 @@ export default class Tabs extends React.Component {
         const clName = this.props.clName
         // console.log("aCTIVE KEY: " + tabData.activeTab.id)
         // console.log("Type: " + typeof(tabData.tabList))
-
+        // console.log("taddata: " + JSON.stringify(tabData))
         return (
             <ul className={clName} role="menu">
             {tabData.tabList.filter(tab =>
                     tab.visible
                 ).map((tab) => {
                     const isActive = tabData.activeTab.id === tab.id
+                    // if(!tab.enabled) console.log("disabled: " + tab.id)
+                    // if(tab.enabled === "false") console.log("enabled: " + tab.id)
+                    // console.log("enabled: " + tab.id + " " + tab.enabled)
+
                 return <li key={tab.id} role="menuitem"
                     style={tab.enabled ? {cursor: "pointer"}: null}
                     className={(isActive ? " active " : " ") + (tab.enabled ? " " : " disabled ") + (tab.notrequired ? " notrequired " : " ")}
