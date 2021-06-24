1149276517357a5cd88b4610c637c0affba6f0fb
bugdriven <helgesandmark@hotmail.com>
2021-06-24 19:30:42 +0200

    created tabItems

---
+++ Prod.Web/src/components/tabItems.js
@@ -0,0 +1,15 @@
+
+function tabItems(tabinfos) {
+    console.log("##" + tabinfos.length)
+    return tabinfos.map(param => {return {
+        id: param.id,
+        name: param.label,
+        get enabled() { return param.enabled === undefined ? true : param.enabled },
+        get visible() { return param.visible === undefined ? true : param.visible },
+        get notrequired() { return param.notrequired === undefined ? false : param.notrequired },
+        url: param.url
+    }})
+
+}
+
+export default tabItems
