172568ad80b93a83f7128dfceb1d3fed190d001d
bugdriven <helgesandmark@hotmail.com>
2021-06-24 19:25:59 +0200

    tabItem updated for mobx 60

--- Prod.Web/src/components/tabItem.js
+++ Prod.Web/src/components/tabItem.js
@@ -1,14 +1,14 @@
-import {extendObservable} from 'mobx'
+import { extendObservable } from 'mobx'
 
 class tabItem {
     constructor(param) {
         extendObservable(this, {
-            'id': param.id,
-            'name': param.label,
-            'enabled': () => param.enabled === undefined ? true : param.enabled,
-            'visible': () => param.visible === undefined ? true : param.visible,
-            'notrequired': () => param.notrequired === undefined ? false : param.notrequired,
-            'url': param.url
+            id: param.id,
+            name: param.label,
+            get enabled() { return param.enabled === undefined ? true : param.enabled },
+            get visible() { return param.visible === undefined ? true : param.visible },
+            get notrequired() { return param.notrequired === undefined ? false : param.notrequired },
+            url: param.url
         })
     }
 }
