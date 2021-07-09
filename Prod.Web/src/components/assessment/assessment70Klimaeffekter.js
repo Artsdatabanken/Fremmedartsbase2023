a5c0cf7252f7dc72582b38e746178430e697f9a1
Svetlana Naumova <svetlana.naumova@artsdatabanken.no>
2021-07-09 13:59:31 +0200

    Fikset klimaeffekter

--- Prod.Web/src/components/assessment/assessment70Klimaeffekter.js
+++ Prod.Web/src/components/assessment/assessment70Klimaeffekter.js
@@ -19,12 +19,23 @@ export default class Assessment70Klimaeffekter extends React.Component {
         return(
             <div>
    				{config.showPageHeaders ? <h3>Klimaeffekter</h3> : <br />}
+                <h3>{climatelabel("heading")}</h3>
                 <fieldset className="well">
-                <Xcomp.StringEnum observableValue={[riskAssessment, 'climateEffectsInvationpotential']} codes={koder.yesNoUnknown} label={climatelabel("climateChangeAffectsInvationPotential")} mode="radio" />
-                <br/>
-                <Xcomp.StringEnum observableValue={[riskAssessment, 'climateEffectsEcoEffect']} codes={koder.yesNoUnknown} label={climatelabel("climateChangeAffectsEcoeffect")} mode="radio" />
-                <br/>
-                <Xcomp.HtmlString observableValue={[riskAssessment, 'climateEffectsDocumentation']} label={climatelabel("climateEffectsDocumentation")}/>
+                    {riskAssessment.riskLevelCode == "NK" ? 
+                    <p>Arten er NK uten usikkerhet oppover, og utfylling av klimaeffekter er dermed ikke aktuelt.</p> : 
+                    <div>
+                    <p>Merk: Tidsperspektivet som lå til grunn for gjeldende delkategorier er vesentlig. 
+                        Det skal være samsvar mellom tidsperspektivene det er svart ut fra under selve risikovurderingen, og tidsperspektivet her. </p>
+
+                        <p>Delkategori invasjonspotensial: 2 (usikkerhet opp mot 3) Gjeldende kriterier: AB </p>
+                        <Xcomp.StringEnum  className="climateChange" observableValue={[riskAssessment, 'climateEffectsInvationpotential']} codes={koder.yesNo} label={climatelabel("climateChangeAffectsInvationPotential")} mode="radio" />
+                        <p>Delkategori økologisk effekt: 3 Gjeldende kriterier: FH</p>
+                        <Xcomp.StringEnum  className="climateChange" observableValue={[riskAssessment, 'climateEffectsEcoEffect']} codes={koder.yesNo} label={climatelabel("climateChangeAffectsEcoeffect")} mode="radio" />
+                        <br/>
+                        <Xcomp.HtmlString observableValue={[riskAssessment, 'climateEffectsDocumentation']} style={{minHeight: '150px'}} label={climatelabel("climateEffectsDocumentation")}/>
+                    </div>
+                }
+                
                 </fieldset>
             </div>
         );
