f240a4cf21a50e395fd7124eae938faad65a7f00
bugdriven <helgesandmark@hotmail.com>
2021-07-14 19:23:57 +0200

    createTaxonSearch - mobx run in action fix

--- Prod.Web/src/components/assessment/52Okologiskeffekt/createTaxonSearch.js
+++ Prod.Web/src/components/assessment/52Okologiskeffekt/createTaxonSearch.js
@@ -1,4 +1,4 @@
-import {autorun} from 'mobx';
+import {autorun, runInAction} from 'mobx';
 // import {loadData} from '../../stores/apiService'; 
 import config from '../../../config';
 
@@ -17,45 +17,49 @@ function loadData() { alert("loaddata Not implemented")}
                 const url = config.taxonApiUrl + searchString
                 // console.log("url:" + url)
                 if(searchString && searchString.length >= 2) {
-                    newObj.taxonSearchWaitingForResult = true
-                    loadData(url, data => {
-                        // const t = JSON.stringify(data)
+                    runInAction(() => {
+                        newObj.taxonSearchWaitingForResult = true
+                        loadData(url, data => {
+                            // const t = JSON.stringify(data)
 
-                        const res = data.
-                            filter(item => item.Species).  // item.ExistsInCountry && 
-                            map(item => {
-                                const taxonRank = item.SubSpecies ? "SubSpecies" : "Species"  
-                                const rlCat = item.TaxonTags.find(tt => tt.Prefix === "RL2015" && tt.Context === contextLetter )
-                                const rlCategory = rlCat ? rlCat.Tag : null
-                                // console.log("tax: " + item.ValidScientificName)
-                                return {
-                                    taxonId: item.TaxonId,
-                                    taxonRank: taxonRank,
-                                    scientificName: item.ValidScientificName,
-                                    scientificNameId: item.ValidScientificNameId,
-                                    author: item.ValidScientificNameAuthorship,
-                                    popularName: item.PrefferedPopularname,
-                                    rlCategory: rlCategory,
-                                    existsInCountry: item.ExistsInCountry
-                                }
-                            }).
-                            filter(item => typeof filter === 'function' ? filter(item) : true )
-                            // map(item => {
-                            //     console.log("tax2: " + item.scientificName)
-                            //     return item
-                            // })
-                        if(newObj.taxonSearchString === searchString) {
-                            //because taxonSearchString may have changed before result is returned
-                            const newList = newObj.taxonSearchString.length >= 2 ? res : []
-                            newObj.taxonSearchWaitingForResult = false
-                            console.log("taxlist length: " + newList.length)
-                            newObj.taxonSearchResult.replace(newList)
-                        }
-                        return null;
+                            const res = data.
+                                filter(item => item.Species).  // item.ExistsInCountry && 
+                                map(item => {
+                                    const taxonRank = item.SubSpecies ? "SubSpecies" : "Species"  
+                                    const rlCat = item.TaxonTags.find(tt => tt.Prefix === "RL2015" && tt.Context === contextLetter )
+                                    const rlCategory = rlCat ? rlCat.Tag : null
+                                    // console.log("tax: " + item.ValidScientificName)
+                                    return {
+                                        taxonId: item.TaxonId,
+                                        taxonRank: taxonRank,
+                                        scientificName: item.ValidScientificName,
+                                        scientificNameId: item.ValidScientificNameId,
+                                        author: item.ValidScientificNameAuthorship,
+                                        popularName: item.PrefferedPopularname,
+                                        rlCategory: rlCategory,
+                                        existsInCountry: item.ExistsInCountry
+                                    }
+                                }).
+                                filter(item => typeof filter === 'function' ? filter(item) : true )
+                                // map(item => {
+                                //     console.log("tax2: " + item.scientificName)
+                                //     return item
+                                // })
+                            if(newObj.taxonSearchString === searchString) {
+                                //because taxonSearchString may have changed before result is returned
+                                const newList = newObj.taxonSearchString.length >= 2 ? res : []
+                                newObj.taxonSearchWaitingForResult = false
+                                console.log("taxlist length: " + newList.length)
+                                newObj.taxonSearchResult.replace(newList)
+                            }
+                            return null;
+                        })
                     })
                 } else {
-                    newObj.taxonSearchWaitingForResult = false
-                    newObj.taxonSearchResult.replace([]) 
+                    runInAction(() => {
+                        newObj.taxonSearchWaitingForResult = false
+                        newObj.taxonSearchResult.replace([]) 
+                    })
                 }
             }
             autorun(() => {doSearch(newObj.taxonSearchString)}, {delay: 300} )
