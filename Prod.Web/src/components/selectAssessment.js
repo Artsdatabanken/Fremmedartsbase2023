661f26617215b162703b2780372670fa419a3d20
Svetlana Naumova <svetlana.naumova@artsdatabanken.no>
2021-10-06 15:52:24 +0200

    Satt karplanter som default expert group

--- Prod.Web/src/components/selectAssessment.js
+++ Prod.Web/src/components/selectAssessment.js
@@ -100,7 +100,7 @@ export default class SelectAssessment extends Component {
     })
 
 
-    findAmountOfAssessments = (appState) => {
+   /* findAmountOfAssessments = (appState) => {
         var result = ""
         if (appState.horizonScanFilter.hsNotStarted) {
             result = appState.getStatisticsFor(appState.assessmentsStatistics,'Progress','2')
@@ -116,7 +116,7 @@ export default class SelectAssessment extends Component {
 
         
         return result
-    }
+    } */
 
     findFilterText = (codes, value) => {
         var result = ""
@@ -135,6 +135,11 @@ export default class SelectAssessment extends Component {
         var experts = ExpertGroupModel.eksperterforvalgtgruppe.filter(item => item.writeAccess == true)
         
         console.log(appState.expertgroupAssessmentList.length)
+        
+        if (appState.expertgroup == "" || appState.expertgroup == undefined) {
+            appState.expertgroup = "Karplanter"
+        }
+        //appState.expertgroup = "Karplanter"
      /*   let checkList = document.getElementById('list1');
         if (checkList) {checkList.getElementsByClassName('anchor')[0].onclick = function (evt) {
             if (checkList.classList.contains('visible'))
@@ -187,6 +192,7 @@ export default class SelectAssessment extends Component {
                                 <Xcomp.StringEnum 
                                         forceSync
                                         observableValue={[appState, 'expertgroup']} 
+                                        
                                         codes={appState.expertgroups}/>                                
                     </div> 
                 </fieldset>
