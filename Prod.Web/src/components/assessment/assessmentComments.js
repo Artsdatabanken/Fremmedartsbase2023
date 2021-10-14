a5ba20f963647fae78b5d31323a072c488945924
Svetlana Naumova <svetlana.naumova@artsdatabanken.no>
2021-10-14 15:16:05 +0200

    Stylet kommentarfanen og flyttet alt som var hardkodet til kodeliste

--- Prod.Web/src/components/assessment/assessmentComments.js
+++ Prod.Web/src/components/assessment/assessmentComments.js
@@ -178,7 +178,7 @@ export default inject('appState')(observer(class AssessmentComments extends Comp
     render() {
         const { appState, appState: { assessment, koder, antallVurderinger } } = this.props
         const rolle = appState.roleincurrentgroup
-                
+        const labels = appState.codeLabels.assessmentComments
         var antallKommentarer = 0;
         this.comments.forEach (entry => {
             if (!entry.closed && !entry.isDeleted){
@@ -191,51 +191,50 @@ export default inject('appState')(observer(class AssessmentComments extends Comp
                 <br></br>
                 <div className="page_wrapper">
                  <div className="form_category">
-                     <h2>Kommentarer</h2>
+                     <h2>{labels.heading}</h2>
                        <div className="form_item">
-                        <table className="table table-striped">
+                        <table className="table table-striped comments">
                                 <thead>
                                     <tr>
-                                        <th>ID</th>
-                                        <th>Kommentarer knyttet til vurderingen</th>
+                                        <th>{labels.id}</th>
+                                        <th>{labels.comments}</th>
                                         <th></th>
-                                        <th>Autor</th>
-                                        <th>Dato</th>
-                                        <th>Slett</th>
+                                        <th>{labels.author}</th>
+                                        <th>{labels.date}</th>
+                                        <th>{labels.delete}</th>
                                     </tr>
                                 </thead>
                                  <tbody>
                             {this.comments &&
                                 this.comments.map((item) => {
                                 return (
-                                <tr key={item.id} style={{borderTop: '1px solid black'}}>
-                                    <td style={{width: '5%', paddingTop: '10px', paddingRight: '10px'}}>{item.id}</td>
+                                <tr key={item.id}>
+                                    <td>{item.id}</td>
                                    
-                                    {item.isDeleted === false ? <td dangerouslySetInnerHTML={{
+                                    {item.isDeleted === false ? <td className="content" dangerouslySetInnerHTML={{
                                          __html: this.linkify(item.comment)
-                                             }}
-                                             style={{width: '45%', paddingTop: '10px', fontFamily: 'Arial'}}>
+                                             }}>
                                     </td> :
-                                     <td style={{width: '45%', paddingTop: '10px'}}> Kommentar er slettet!
+                                     <td className="content"> {labels.isDeleted}
                                      </td>}
-                                    <td style={{width: '10%', paddingTop: '10px'}}>{item.closed === true && <span style={{color: 'red'}}>Behandlet</span>}</td>                                           
-                                    {item.isDeleted === false ? <td style={{width: '20%', paddingTop: '10px'}}>
+                                    <td>{item.closed === true && <span> {labels.processed}</span>}</td>                                           
+                                    {item.isDeleted === false ? <td>
                                         {item.user}
-                                    </td> : <td style={{width: '20%', paddingTop: '10px'}}>
+                                    </td> : <td>
                                     </td>}
-                                    {item.isDeleted === false ? <td style={{width: '20%', paddingTop: '10px'}}>
+                                    {item.isDeleted === false ? <td>
                                         {item.commentDate.substring(0,10)}
-                                    </td> : <td style={{width: '20%', paddingTop: '10px'}}>
+                                    </td> : <td>
                                     </td>}
-                                    {item.isDeleted === false ? <td style={{display: 'flex', width: '100%', paddingTop: '10px'}}>
+                                    {item.isDeleted === false ? <td>
                                     <button className="btn btn-primary btn-xs" disabled={item.userId != auth.user.profile.sub} onClick={() => {this.deleteComment(item.id, appState)}}>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
                                         <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
                                         <path fillRule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
                                     </svg>
                                     </button>                                   
-                                    { (rolle.admin || rolle.skriver) && <button className="btn btn-primary btn-xs" disabled={item.closed}  onClick={() => {this.closeComment(item.id, appState)}}>Behandlet</button> }                                                                       
-                                </td> : <td style={{display: 'flex', width: '100%', paddingTop: '10px'}}>                                    
+                                    { (rolle.admin || rolle.skriver) && <button className="btn btn-primary btn-xs" disabled={item.closed}  onClick={() => {this.closeComment(item.id, appState)}}>{labels.processed}</button> }                                                                       
+                                </td> : <td>                                    
                                 </td>}
                                 </tr>                             
                             )})}
@@ -246,16 +245,16 @@ export default inject('appState')(observer(class AssessmentComments extends Comp
 
                         <div className="form_category">
                             <div className="container_description">
-                                <p>Her kan du skrive kommentar på vurderingen.</p>
+                                <p>{labels.writeCommentHere}</p>
                             </div>
 
                         <Xcomp.HtmlString disabled={false} 
-                            style={{height: '35%', fontFamily: 'Arial', fontSize: '14px'}}
+                            className={"commentField"}
                             editable={true}
                             observableValue={[appState, 'newComment']}
                             // // // codes={koder.tilbakemeldinger} mode="radio"
                         />
-                        <button className="btn btn-primary" style={{marginTop: '20px'}} onClick={() => { this.addComment(appState.newComment, appState)}}>Send</button>
+                        <button className="btn btn-primary sendComment" onClick={() => { this.addComment(appState.newComment, appState)}}>{labels.send}</button>
                     
                 </div> 
             </div>
