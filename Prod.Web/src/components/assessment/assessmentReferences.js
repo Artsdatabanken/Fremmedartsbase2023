29a0dfaf0b42679949b1a370d8aa888b7dcbf2b5
Svetlana Naumova <svetlana.naumova@artsdatabanken.no>
2021-10-29 22:34:23 +0200

    Prøvde å fikse oppdatering av referanser igjen

--- Prod.Web/src/components/assessment/assessmentReferences.js
+++ Prod.Web/src/components/assessment/assessmentReferences.js
@@ -229,8 +229,8 @@ export default inject('appState')(observer(class AssessmentReferences extends Co
         if (clone.id === "NY_REFERANSE") {
             clone.id = "00000000-0000-0000-0000-000000000000"
             postData(
-                //config.getUrl("Reference"),
-                config.referenceApiUrl+"api/References",
+                config.getUrl("Reference"),
+                //config.referenceApiUrl+"api/References",
                 clone,
                 data => {
                     this.updateValgtReferanse(data)
@@ -244,9 +244,10 @@ export default inject('appState')(observer(class AssessmentReferences extends Co
             )
             this.addNew = true
         } else {
+            console.log(config.getUrl("References/" + clone.id))
             putData(
-                //config.getUrl("Reference/" + clone.id),
-                config.referenceApiUrl+"api/References/" + clone.id,
+                config.getUrl("References/" + clone.id),
+                //config.referenceApiUrl+"api/References/" + clone.id,
                 clone,
                 data => {
                     this.updateValgtReferanse(data)
@@ -257,8 +258,7 @@ export default inject('appState')(observer(class AssessmentReferences extends Co
                     //sokeresultat([{ id: data.id, type: data.type, referenceString: data.referenceString }]);
                     //console.log(sokeresultat)
                 }
-            )
-            console.log("Referanse oppdatert!")
+            )            
         }
         //this.editMode = false
         this.valgtReferanse.allowDelete = true
@@ -491,7 +491,7 @@ export default inject('appState')(observer(class AssessmentReferences extends Co
                                             <td><span>{referanse.type}</span> </td>
                                             <td><Xcomp.Button className="btn btn-primary btn-xs"  title={labels.General.remove} onClick={() => {this.removeReferenceFromAssessment(assessment, referanse.referenceId)}}>
                                                 {/*{labels.references.remove}*/}
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
                                                     <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
                                                     <path fillRule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
                                                 </svg>
