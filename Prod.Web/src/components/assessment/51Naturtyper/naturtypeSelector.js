b3f160154c30ae75bec1dbbd80cd0c47ebd5e8ae
bugdriven <helgesandmark@hotmail.com>
2021-11-22 16:17:02 +0100

    naturtypeSelector uses config object for different naturetypes

--- Prod.Web/src/components/assessment/51Naturtyper/naturtypeSelector.js
+++ Prod.Web/src/components/assessment/51Naturtyper/naturtypeSelector.js
@@ -21,7 +21,7 @@ export default class NaturtypeSelector extends React.Component {
             : kode
     }
     render() {
-        const {naturtyper, setSelected} = this.props;
+        const {naturtyper, setSelected, config} = this.props;
 
         // console.log("SELECTOR: " + JSON.stringify(naturtyper, undefined, 2))
 
@@ -38,9 +38,10 @@ export default class NaturtypeSelector extends React.Component {
                         </div>
                         <div className="tree-view-label">
                             <span className="hovedtypegruppe">
-                                <span className="naturtype-kode" style={{width: "30px"}}>{this.truncCode(hovedtypegruppe.Value)}</span>
+                                <span className="naturtype-kode" style={{width: config.codewidth}}>{this.truncCode(hovedtypegruppe.Value)}</span>
                                 {/* to show the name for the highest level for those nature types that only have code*/}
-                                {this.truncCode(hovedtypegruppe.Value).length < 3 && <span>{hovedtypegruppe.Text}</span>}
+                                {/* {this.truncCode(hovedtypegruppe.Value).length < 3 && <span>{hovedtypegruppe.Text}</span>} */}
+                                {config.showdescription(this.truncCode(hovedtypegruppe.Value)) && <span>{hovedtypegruppe.Text}</span>}
                                {/* <span>{hovedtypegruppe.Id}</span> */}
                             </span>
                         </div>
