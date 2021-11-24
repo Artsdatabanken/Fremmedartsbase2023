639f23e05dafa773420d170db3871b9b44a14f06
bugdriven <helgesandmark@hotmail.com>
2021-11-24 17:30:41 +0100

    truetSelector add selector hierarchy on experttema

--- Prod.Web/src/components/assessment/51Naturtyper/truetSelector.js
+++ Prod.Web/src/components/assessment/51Naturtyper/truetSelector.js
@@ -34,19 +34,33 @@ export default class TruetSelector extends React.Component {
             <div>
                 {naturtyper.map(hovedtypegruppe => 
                     <div key={hovedtypegruppe.Id}>
-                        {/* <div
-                        className={"glyphicon glyphicon-chevron-down tree-view-arrow " }
+                        <div
+                            className={"glyphicon glyphicon-chevron-down tree-view-arrow " }
                             onClick={action(() => hovedtypegruppe.Collapsed = !hovedtypegruppe.Collapsed)} 
-                            > 
-                            {hovedtypegruppe.Collapsed == false ? <ExpandMoreIcon/> : <NavigateNextIcon/>}
+                            > {hovedtypegruppe.Collapsed == false ? <ExpandMoreIcon/> : <NavigateNextIcon/>}
                             
-                        </div> */}
-                        <div className="tree-view-label btn-flat" onClick={() => this.setSelectedNT(hovedtypegruppe.Id)}>
+                        </div>
+                        <div className="tree-view-label">
                             <span className="hovedtypegruppe">
-                                <span className="naturtype-kode" style={{width: "350px"}}>{hovedtypegruppe.Text}</span>
-                                {<span>{hovedtypegruppe.Value}</span>}
+                                <span className="naturtype-kode" style={{width: "300px"}}>{hovedtypegruppe.Value}</span>
+                                {/* <span>{hovedtypegruppe.Text}</span>} */}
                             </span>
                         </div>
+                        {
+                        !hovedtypegruppe.Collapsed && hovedtypegruppe.Children ?
+                        <div className="tree-view-children">
+                            {hovedtypegruppe.Children.map(hovedtype =>
+
+                            <div key={hovedtype.Id} className="tree-view-label btn-flat" onClick={() => this.setSelectedNT(hovedtype.Id)}>
+                                <span className="hovedtypegruppe">
+                                    <span className="naturtype-kode" style={{width: "350px"}}>{hovedtype.Text}</span>
+                                    {<span>{hovedtype.Value}</span>}
+                                </span>
+                            </div>
+                            )}
+                        </div> :
+                        null 
+                        }
                     </div>
                 )}
             </div> 
