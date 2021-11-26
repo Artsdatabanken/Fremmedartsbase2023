a61f72ea068575abc571124d8c7a98c60f7f8827
bugdriven <helgesandmark@hotmail.com>
2021-11-26 08:49:57 +0100

    naturetypeSelector now supports kartleggingsenheter

--- Prod.Web/src/components/assessment/51Naturtyper/naturtypeSelector.js
+++ Prod.Web/src/components/assessment/51Naturtyper/naturtypeSelector.js
@@ -47,12 +47,34 @@ export default class NaturetypeSelector extends React.Component {
                         !hovedtypegruppe.Collapsed && hovedtypegruppe.Children ?
                         <div className="tree-view-children">
                             {hovedtypegruppe.Children.map(hovedtype =>
-
-                            <div key={hovedtype.Id} className="tree-view-label btn-flat" onClick={() => this.setSelectedNT(hovedtype.Id)}>
-                                <div className="hovedtypegruppe">
+                            <div key={hovedtype.Id}>
+                                <div
+                                    className={"glyphicon glyphicon-chevron-down tree-view-arrow " + 
+                                        (hovedtype.Collapsed ? "tree-view-arrow-collapsed" : "")}
+                                    onClick={() => hovedtype.Collapsed = !hovedtype.Collapsed}>
+                                    {hovedtype.Children.length > 0 ? hovedtype.Collapsed == false? <ExpandMoreIcon/> : <NavigateNextIcon/> : <div style={{width: '24px'}}></div>} {/* <-- to align all the choice buttons even though they don't have an arrow to expand */}
+                                </div>
+                                <div className="hovedtype tree-view-label btn-flat"  onClick={() => this.setSelectedNT(hovedtype.Id)}>
                                     <span className="naturtype-kode">{hovedtype.Text}</span>
                                     <span className="nt-code">{"'"+hovedtype.Value+"'"}</span>
                                 </div>
+                                {!hovedtype.Collapsed && hovedtype.Children ?
+                                <div className="tree-view-children">
+                                {hovedtype.Children.map(grunntype =>
+                                    <div key={grunntype.Id} onClick={() => this.setSelectedNT(grunntype.Id)}>
+                                        <span className="grunntype btn-flat">
+                                            <span className="naturtype-kode">{grunntype.Id}</span>
+                                            {/* <span>{grunntype.name}</span> */}
+                                            <span>{grunntype.Text}</span>
+                                        </span>
+                                    </div>
+                                )}
+                                </div> :
+                                null }
+
+
+
+
                             </div>
                             )}
                         </div> :
