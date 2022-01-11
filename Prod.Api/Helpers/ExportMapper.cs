b0c08c32e4ac7e27592f7ce054f95bc76cd5c727
Stein Arild Hoem <stein.hoem@artsdatabanken.no>
2022-01-11 11:17:34 +0100

    #330 start på kongefila....

--- Prod.Api/Helpers/ExportMapper.cs
+++ Prod.Api/Helpers/ExportMapper.cs
@@ -19,7 +19,7 @@ namespace Prod.Api.Helpers
                     .ForMember(x => x.DoorKnockerType, opt => opt.MapFrom(src => GetDoorknockerType(src)))
                     ;
                 cfg.CreateMap<FA4WithComments, FA4Export>()
-                    .ForMember(x => x.DoorKnockerType, opt => opt.MapFrom(src => GetDoorknockerType(src)))
+                    //.ForMember(x => x.DoorKnockerType, opt => opt.MapFrom(src => GetDoorknockerType(src)))
                     ;
             });
             var mapper = new Mapper(mapperConfig);
@@ -67,7 +67,16 @@ namespace Prod.Api.Helpers
     {
         public int Id { get; set; }
         public string ExpertGroup { get; set; }
-        public string DoorKnockerType { get; set; }
+        public string EvaluationContext { get; set; }        
+        public bool IsEvaluated { get; set; }  // ???
+        //public bool IsDeleted { get; set; }
+        public string EvaluationStatus { get; set; }
+
+        public string TaxonHierarcy { get; set; }
+
+        //public Datasett Datasett { get; set; } = new Datasett();
+        public string EvaluatedScientificNameRank { get; set; }
+        //public string DoorKnockerType { get; set; }
 
         public string EvaluatedScientificNameId { get; set; }
 
@@ -76,9 +85,180 @@ namespace Prod.Api.Helpers
 
         public string EvaluatedVernacularName { get; set; }
         public string EvaluatedScientificNameAuthor { get; set; }
-
+        public DateTime LastUpdatedAt { get; set; }
         [Name("SistOppdatertAv")]
         public string LastUpdatedBy { get; set; }
+        
+
+        public int TaxonId { get; set; }
+
+        public string Citation { get; set; }
+        public string AlienSpeciesCategory { get; set; }
+        //public List<string> ReasonForChangeOfCategory { get; set; } = new List<string>();
+
+        public DateTime LockedForEditAt { get; set; }
+        public string LockedForEditBy { get; set; }
+
+        // (3.1) Artens status{
+        public string AlienSpeciesCategory2012 { get; set; } // added 10.01.2017
+        public string DoorKnockerDescription { get; set; } // fab: Door_Knocker_Description
+        public string NotReproductiveDescription2012 { get; set; } // fab: Not_Reproductive_Description 
+        public string NotReproductiveFutureDescription2012 { get; set; } // fab: Not_Reproductive_Future_Description
+        public string AssesmentNotApplicableDescription { get; set; } // fab: Assesment_Not_Applicable_Description
+
+        public bool? IsAlienSpecies { get; set; }
+
+        public string IsAlien { get; set; } // new in 2021
+        public bool? IsRegionallyAlien { get; set; }
+
+        public bool? ConnectedToAnother { get; set; }
+
+        public string Connected { get; set; }
+
+        public string SpeciesStatus { get; set; }
+
+        public string ConnectedTaxon1 { get; set; } = "";
+
+        public string ConnectedTaxon2 { get; set; } = "";
+
+        public bool? ProductionSpecies { get; set; } = false;
+
+        public string ChangedFromAlien { get; set; }
+
+        public string ChangedAssessment { get; set; }
+
+        public bool? AlienSpecieUncertainIfEstablishedBefore1800 { get; set; } // lagt til: 19.10.2016 - renamed 15.11.2016
+        public bool AlienSpecieUncertainAntropochor { get; set; } // lagt til: 19.10.2016
+        public string AlienSpecieUncertainDescription { get; set; } // lagt til: 22.12.2016
+
+        public bool SkalVurderes { get; set; }
+
+        //public string EtableringsmulighetINorge { get; set; } // lagt til 22.08.2016 //fjernet 25.08.2016
+        public string DoorKnockerCategory { get; set; } // lagt til 25.08.2016 // fjernet 14.12.2016 // lagt til 21.12.2016
+        public string RegionallyAlienCategory { get; set; } // lagt til 25.08.2016
+        public string NotApplicableCategory { get; set; } // lagt til 25.08.2016
+        //public bool LongDistanceEffect { get; set; } // lagt til 22.08.2016 // fjernet 29.08.2016
+        //public bool IndoorObserved { get; set; } // lagt til 22.08.2016 // fjernet 30.08.2016
+        //public bool IndoorEstablished { get; set; } // lagt til 22.08.2016 // fjernet 30.08.2016
+        //public bool OutdoorObserved { get; set; } // lagt til 22.08.2016 // fjernet 30.08.2016
+        //public bool OutdoorEstablished { get; set; } // lagt til 22.08.2016 // fjernet 30.08.2016
+        //public bool NorwegianNatureObserved { get; set; } // lagt til 22.08.2016 // fjernet 30.08.2016
+        //public bool NorwegianNatureEstablished { get; set; } // lagt til 22.08.2016 // fjernet 30.08.2016
+
+        //public class TimeAndPlace
+        //{
+        //    public string Place { get; set; }
+        //    public string Time { get; set; }
+        //}
+        //public class ObservedAndEstablished
+        //{
+        //    public TimeAndPlace ObservedInCountry { get; set; } = new TimeAndPlace();
+        //    public TimeAndPlace FertileSpecimenObserved { get; set; } = new TimeAndPlace();
+        //    public TimeAndPlace Established { get; set; } = new TimeAndPlace();
+        //    public TimeAndPlace Population { get; set; } = new TimeAndPlace();
+        //    public string SpecimenCount { get; set; } // lagt til 26.09.2016
+        //}
+        //public class ObservedAndEstablishedInCountry
+        //{
+        //    public ObservedAndEstablished Indoor { get; set; } = new ObservedAndEstablished();
+        //    public ObservedAndEstablished ProductionArea { get; set; } = new ObservedAndEstablished();
+        //    public ObservedAndEstablished NorwegianNature { get; set; } = new ObservedAndEstablished();
+        //}
+
+        // todo: pakke ut denne flatt
+        //public ObservedAndEstablishedInCountry ObservedAndEstablishedStatusInCountry { get; set; } = new ObservedAndEstablishedInCountry(); // lagt til 30.08.2016 //
+
+        // todo: fylkesforekomster...
+
+        // (3.2) Artsegenskaper
+    
+        //public string LimnicTerrestrialMarine { get; set; } // lagt til 2.9.2016 // fjernet 26.09.2016
+        public bool Limnic { get; set; } // lagt til 26.9.2016
+        public bool Terrestrial { get; set; } // lagt til 26.9.2016
+        public bool Marine { get; set; } // lagt til 26.9.2016
+        public bool BrackishWater { get; set; } // lagt til 26.9.2017 (for sfab)
+
+        public string FirstDomesticObservation { get; set; } // fab: First_Domestic_Observation
+        public string FirstDomesticObservationLocation { get; set; } // fab: First_Domestic_Observation_Location
+        public string FirstDomesticEstablishmentObserved { get; set; } // fab: First_Domestic_Observed_Establishment
+        public string FirstDomesticEstablishmentObservedLocation { get; set; } // fab: First_Domestic_Observed_Establishment_Location
+
+
+        public List<string> ArrivedCountryFrom { get; set; } = new List<string>(); // fab: string Arived_Norway_From_Code
+        public string ArrivedCountryFromDetails { get; set; } = ""; // fab: Natural_Origin 'NaturalOrigin'  - lagt til 14.11.2016
+
+
+        //public List<NaturalOrigin> NaturalOrigins { get; set; } = new List<NaturalOrigin>(); // lagt til 09.01.2017
+        public string NaturalOriginUnknownDocumentation { get; set; }
+        //public List<NaturalOrigin> CurrentInternationalExistenceAreas { get; set; } = new List<NaturalOrigin>(); // lagt til 09.01.2017
+        public string CurrentInternationalExistenceAreasUnknownDocumentation { get; set; }
+
+        //public NaturalOrigin createDefaultNaturalOrigin(string climateZone, string climateZoneSubType)
+        //{
+        //    return new NaturalOrigin()
+        //    {
+        //        ClimateZone = climateZone,
+        //        ClimateZoneSubtype = climateZoneSubType,
+        //    };
+        //}
+        //public List<NaturalOrigin> createDefaultNaturalOrigins()
+        //{
+        //    return new List<NaturalOrigin> {
+        //        createDefaultNaturalOrigin("polart","inkl alpint"),
+        //        createDefaultNaturalOrigin("temperert","boreal"),
+        //        createDefaultNaturalOrigin("temperert","nemoral"),
+        //        createDefaultNaturalOrigin("temperert","tørt"),
+        //        createDefaultNaturalOrigin("subtropisk","middelhavsklima"),
+        //        createDefaultNaturalOrigin("subtropisk","fuktig"),
+        //        createDefaultNaturalOrigin("subtropisk","tørt"),
+        //        createDefaultNaturalOrigin("subtropisk","høydeklima"),
+        //        createDefaultNaturalOrigin("subtropisk","kappregionen"),
+        //        createDefaultNaturalOrigin("tropisk","")
+        //    };
+        //}
+
+        public List<string> NaturalOriginMarine { get; set; } = new List<string>(); // lagt til 05.09.2016
+        public string NaturalOriginMarineDetails { get; set; } // lagt til 21.04.2017
+        public List<string> CurrentInternationalExistenceMarineAreas { get; set; } = new List<string>(); // lagt til 05.09.2016
+        public string CurrentInternationalExistenceMarineAreasDetails { get; set; } // lagt til 21.04.2017
+
+        public bool SurvivalBelow5c { get; set; } // lagt til 27.09.2016
+
+        public List<string> IntroductionCourse { get; set; } // fab: List<int> Introduction_Course  //Årsak til tilstedeværelse
+
+        public bool? DomesticRiskEvaluationExists { get; set; } // fab: Domestic_RiskEvaluation_Exists
+        public bool? DomesticRiskEvaluationExists2007 { get; set; } // lagt til 26.09.2016
+        public bool? ForeignRiskEvaluationExists { get; set; } // fab: Foreign_RiskEvaluation_Exists_Exists
+        public string ForeignRiskEvaluationComment { get; set; } // fab: Foreign_RiskEvaluation_Comment
+
+        public string Regeneration { get; set; } // fab: Regeneration
+        public int? RegenerationYears { get; set; } // fab: Regeneration_Years
+        public string Reproduction { get; set; } // fab: Reproduction
+        public bool? ReproductionAsexual { get; set; } // fab: Reproduction_Asexual
+        public double? ReproductionGenerationTime { get; set; } // fab: Reproduction_Geteration_Time
+        public bool? ReproductionSexual { get; set; } // fab: Reproduction_Sexual
+        //public string ReproductiveCapability { get; set; } // fab: Int64? Reproductive_Capability // removed 17.01.2017
+        public bool? SimilarDemographicComparison { get; set; } // fab: Similar_Demographic_Comparison
+
+
+
+        //Ikke-vurderte effekter  - Har vi disse fra 2012?
+        public string HealthEffects { get; set; } // lagt til 31.08.2016
+        public string EconomicEffects { get; set; } // lagt til 31.08.2016
+        //public string EcosystemServiceEffects { get; set; } // lagt til 31.08.2016 // fjernet 16.09.2016
+        public List<string> EcosystemServiceEffectsBasicLifeProcesses { get; set; } = new List<string>(); // lagt til 02.09.2016
+        public List<string> EcosystemServiceEffectsProvisioningServices { get; set; } = new List<string>(); // lagt til 02.09.2016
+        public List<string> EcosystemServiceEffectsRegulatingServices { get; set; } = new List<string>(); // lagt til 02.09.2016
+        public List<string> EcosystemServiceEffectsHumanSpiritualServices { get; set; } = new List<string>(); // :-) // lagt til 02.09.2016
+        public string PositiveEcologicalEffects { get; set; } // lagt til 31.08.2016
+        public string EffectsOnPopulationOfOrigin { get; set; } // lagt til 31.08.2016
+
+        // OsA, He, Fi
+        public string RegionalPresenceKnown { get; set; }
+        public string RegionalPresenceAssumed { get; set; }
+        public string RegionalPresencePotential { get; set; }
+        // slutt artsegenskaper
+
 
 
         // comments
