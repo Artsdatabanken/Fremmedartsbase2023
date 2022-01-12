145ae9b45f73776c7c08d9155bcf80497fefb040
Stein Arild Hoem <stein.hoem@artsdatabanken.no>
2022-01-12 15:22:33 +0100

    Fix #342

--- Prod.Api/Helpers/IndexHelper.cs
+++ Prod.Api/Helpers/IndexHelper.cs
@@ -22,7 +22,7 @@ namespace Prod.Api.Helpers
         /// <summary>
         ///     Change this to force index rebuild!
         /// </summary>
-        public const int IndexVersion = 28;
+        public const int IndexVersion = 29;
 
         private const string Field_Id = "Id";
         private const string Field_Group = "Expertgroup";
