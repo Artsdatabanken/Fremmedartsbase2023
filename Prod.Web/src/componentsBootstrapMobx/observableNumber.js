9548dc5e9360c484b538639bf5e126e43497a441
bugdriven <helgesandmark@hotmail.com>
2022-01-31 14:05:55 +0100

    observableNumber cleanup

--- Prod.Web/src/componentsBootstrapMobx/observableNumber.js
+++ Prod.Web/src/componentsBootstrapMobx/observableNumber.js
@@ -5,79 +5,6 @@ import { Observer } from 'mobx-react'
 
 import {UserContext} from './components'
 
-// @observer
-// export default class ObservableNumber extends React.Component {
-//   context = useUserContext()
-//   render() {
-//     const { observableValue, validate, integer, label, width, disabled } = this.props
-//     const [obj, prop] = observableValue
-//     // const obstype = typeof(obj[prop])
-//     //console.log("obstype (" + prop + "):" + obstype + "  value: " + obj[prop])
-
-//     const hasLabel = !!label
-//     const isInvalid =
-//       validate && typeof validate === 'function' && !validate(obj[prop])
-
-//     console.log("context: " + JSON.stringify(this.context) + "#" + this.props.readonly + "¤" + this.eeee )
-//     // console.log("config: " + JSON.stringify(this.context) + "#" + this.props.readonly + "¤" + this.eeee )
-
-
-//     return (
-//       <div className={isInvalid ? 'has-error' : null}>
-//         <span>
-//           {hasLabel && <label htmlFor={prop}>{label}</label>}
-//           <input
-//             className="form-control"
-//             name={prop}
-//             value={ObservableNumber.filterDisplay(obj, prop)}
-//             onChange={action(e => {
-//               obj[prop] = ObservableNumber.filterNumericInput(
-//                 e.currentTarget.value,
-//                 integer
-//               )
-//             })}
-//             disabled={this.context.readonly || disabled}
-//             style={{ width }}
-//           />
-//         </span>
-//       </div>
-//     )
-//   }
-
-//   static filterDisplay(obj, prop) {
-//     if (obj[prop] || obj[prop] === 0)
-//       return ObservableNumber.filterNumericInput(obj[prop].toString())
-//     return ''
-//   }
-
-//   static filterNumericInput(s, integer = false) {
-//     let r = ''
-//     const digits = '0123456789'
-//     let decimalSeparators = integer ? '' : '.,'
-
-//     let decimalPointOffset = undefined
-//     for (const i in s) {
-//       const c = s[i]
-//       if (digits.indexOf(c) >= 0) r += c
-//       else if (!decimalPointOffset && decimalSeparators.indexOf(c) >= 0)
-//         decimalPointOffset = r.length
-//     }
-//     if (decimalPointOffset < s.length)
-//       r = r.slice(0, decimalPointOffset) + ',' + r.slice(decimalPointOffset)
-//     if (decimalPointOffset === 0) r = '0' + r
-//     return r
-//   }
-// }
-
-// ObservableNumber.contextTypes = {
-//   readonly: PropTypes.bool
-// }
-
-// ObservableNumber.propTypes = {
-//   integer: PropTypes.bool,
-//   observableValue: PropTypes.array.isRequired
-// }
-
 function filterDisplay(obj, prop, yearRange) {
   if (obj[prop] || obj[prop] === 0)
     if (obj[prop] === 'ERROR!')
@@ -95,7 +22,7 @@ function getErrors(observableErrors) {
     const errorhandler = observableErrors[0]
     const errors = []
     for(const key of errorKeys) {
-      console.log("#%% errorkey: " + key + " value: " + errorhandler.errors[key])
+      // console.log("#%% errorkey: " + key + " value: " + errorhandler.errors[key])
       if(errorhandler.errors[key]) {
         errors.push(errorhandler.errors[key])
       }
