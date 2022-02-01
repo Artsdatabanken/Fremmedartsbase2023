cef4196cbd48a1d12f2e6048af995605170d9a6c
bugdriven <helgesandmark@hotmail.com>
2022-02-01 12:27:30 +0100

    refactor - new file errorDefinitions.js

--- Prod.Web/src/components/assessment/enhanceCriteria.js
+++ Prod.Web/src/components/assessment/enhanceCriteria.js
@@ -3,7 +3,7 @@ import RiskLevel from './riskLevel';
 import {extractFloat, getCriterion} from '../../utils'
 import { EventNote } from '@material-ui/icons';
 import config from '../../config';
-import errorhandler from '../errorhandler';
+// import errorhandler from '../errorhandler';
 import { JsonHubProtocol } from '@microsoft/signalr';
 
 function round(num){return Math.round(num)}
@@ -582,81 +582,7 @@ function enhanceRiskAssessmentInvasjonspotensiale(riskAssessment) {
     })
     autorun(() => {
         console.log("ascore: " + r.ascore )
-
-
-
     })
-
-    errorhandler.addErrors([{
-        id: "(a)err1", 
-        get cond() {return r.AOOtotalBest === 0},
-        msg: "En selvstendig reproduserende art må ha et forekomstareal på minst 4_km²!"
-    },
-    {
-        id: "(a)err2", 
-        get cond() {return r.AOOtotalLow > r.AOOtotalBest},
-        msg: "Det nedre anslaget på forekomstarealet kan ikke være større enn det beste anslaget!"
-    },
-    {
-        id: "(a)err3", 
-        get cond() {return r.AOOtotalHigh < r.AOOtotalBest},
-        msg: "Det øvre anslaget på forekomstarealet kan ikke være mindre enn det beste anslaget!"
-    },
-    {
-        id: "(a)err4", 
-        get cond() {return r.AOO50yrLow > r.AOO50yrBest},
-        msg: "Det nedre anslaget på forekomstarealet kan ikke være større enn det beste anslaget!"
-    },
-    {
-        id: "(a)err5", 
-        get cond() {return r.AOO50yrHigh < r.AOO50yrBest},
-        msg: "Det øvre anslaget på forekomstarealet kan ikke være mindre enn det beste anslaget!"
-    },
-    {
-        id: "(a)err6", 
-        get cond() {return r.AOOtotalBest < r.AOOknown},
-        msg: "Det beste anslaget på det totale nåværende forekomstarealet kan ikke være mindre enn det kjente!"
-    },
-    {
-        id: "(b)err1", 
-        get cond() {return r.occurrences1Low > r.occurrences1Best},
-        msg: "Det nedre anslaget på antall forekomster kan ikke være større enn det beste anslaget!"
-    },
-    {
-        id: "(b)err2", 
-        get cond() {return r.occurrences1High < r.occurrences1Best},
-        msg: "Det øvre anslaget på antall forekomster kan ikke være mindre enn det beste anslaget!"
-    },
-    {
-        id: "Aerr1", 
-        get cond() {return r.ametodkey === "AmethodInvalid"},
-        msg: "Metode for A-kriteriet er ikke valgt."
-    },
-    {
-        id: "A3err1", 
-        get cond() {return r.lifetimeLowerQ > r.medianLifetime},
-        msg: "Levetidens nedre kvartil må være mindre enn medianen."
-    },
-    {
-        id: "A3err2", 
-        get cond() {return r.lifetimeUpperQ <= r.medianLifetime},
-        msg: "Levetidens øvre kvartil må være større enn medianen."
-    },
-    {
-        id: "Berr1", 
-        get cond() {return (r.bmetodkey === "BmethodNotChosen" || r.bmetodkey === "B2bX") },
-        msg: "Metode for B-kriteriet er ikke valgt."
-    },
-    {
-        id: "B1err1", 
-        get cond() {return r.expansionLowerQ > r.expansionSpeed},
-        msg: "Ekspansjonshastighetens nedre kvartil må være mindre enn medianen."
-    },
-    {
-        id: "B1err2", 
-        get cond() {return r.expansionUpperQ <= r.expansionSpeed},
-        msg: "Ekspansjonshastighetens øvre kvartil må være større enn medianen."
-    }])
 
     const ACriteriaSectionNames = [
         "LifespanA1aSimplifiedEstimate", // changed from  "SpreadPVAAnalysisEstimatedSpeciesLongevity" 11.07.21
