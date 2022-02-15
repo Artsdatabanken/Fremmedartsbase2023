2be42ceb3f44a9f382e574bff5bcaea6f7a7d298
Svetlana Naumova <svetlana.naumova@artsdatabanken.no>
2022-02-15 17:38:40 +0100

    Removed risk assessment error messages for species that are not going to be assessed

--- Prod.Web/src/components/assessment/errorDefinitions.js
+++ Prod.Web/src/components/assessment/errorDefinitions.js
@@ -14,117 +14,117 @@
         },
         {
             id: "(a)err2",
-            get cond() {return r.AOOtotalLow > r.AOOtotalBest},
+            get cond() {return r.doFullAssessment && (r.AOOtotalLow > r.AOOtotalBest)},
             msg: "Det nedre anslaget på forekomstarealet kan ikke være større enn det beste anslaget!"
         },
         {
             id: "(a)err3",
-            get cond() {return r.AOOtotalHigh < r.AOOtotalBest},
+            get cond() {return r.doFullAssessment && (r.AOOtotalHigh < r.AOOtotalBest)},
             msg: "Det øvre anslaget på forekomstarealet kan ikke være mindre enn det beste anslaget!"
         },
         {
             id: "(a)err4",
-            get cond() {return r.AOO50yrLow > r.AOO50yrBest},
+            get cond() {return r.doFullAssessment && (r.AOO50yrLow > r.AOO50yrBest)},
             msg: "Det nedre anslaget på forekomstarealet kan ikke være større enn det beste anslaget!"
         },
         {
             id: "(a)err5",
-            get cond() {return r.AOO50yrHigh < r.AOO50yrBest},
+            get cond() {return r.doFullAssessment && (r.AOO50yrHigh < r.AOO50yrBest)},
             msg: "Det øvre anslaget på forekomstarealet kan ikke være mindre enn det beste anslaget!"
         },
         {
             id: "(a)err6",
-            get cond() {return r.AOOtotalBest < r.AOOknown},
+            get cond() {return r.doFullAssessment && (r.AOOtotalBest < r.AOOknown)},
             msg: "Estimatet kan ikke justeres til å være lavere enn kjent forekomstareal!"
         },
         {
             id: "(a)err7",
-            get cond() {return r.AOO1 < r.AOOknown1},
+            get cond() {return r.doFullAssessment && (r.AOO1 < r.AOOknown1)},
             msg: "Det beste anslaget på det totale nåværende forekomstarealet kan ikke være mindre enn det kjente!"
             //for ">":  "MERK: Estimatet skal kun justeres om forekomster som er utgått pga. tiltak ble tatt ut av estimatet for kjent forekomstareal"
         },
         {
             id: "(a)err8",
-            get cond() {return r.AOO2 < r.AOOknown2},
+            get cond() {return r.doFullAssessment && (r.AOO2 < r.AOOknown2)},
             msg: "Det beste anslaget på det totale nåværende forekomstarealet kan ikke være mindre enn det kjente!"
         },
         {
             id: "(a)err9",
-            get cond() {return r.bmetodkey === "B2a" && AOOfirstOccurenceLessThan10Years === "yes" && r.AOO1 <= 0},
+            get cond() {return r.doFullAssessment && (r.bmetodkey === "B2a" && AOOfirstOccurenceLessThan10Years === "yes" && r.AOO1 <= 0)},
             msg: "Forekomstarealet kan ikke være 0!"
         },
         {
             id: "(a)err10",
-            get cond() {return r.bmetodkey === "B2a" && AOOfirstOccurenceLessThan10Years === "yes" && r.AOO2 <= 0},
+            get cond() {return r.doFullAssessment && (r.bmetodkey === "B2a" && AOOfirstOccurenceLessThan10Years === "yes" && r.AOO2 <= 0)},
             msg: "Forekomstarealet kan ikke være 0!"
         },
         {
             id: "(a)err11",
-            get cond() {return !r.doorKnocker && ((r.AOOknownInput % 4) !== 0) },
+            get cond() {return r.doFullAssessment && (!r.doorKnocker && ((r.AOOknownInput % 4) !== 0) )},
             msg: "Forekomstarealene må være multippel av 4_km²!"
         },
         {
             id: "(a)err12",
-            get cond() {return !r.doorKnocker && ((r.AOOtotalLowInput % 4) !== 0) },
+            get cond() {return r.doFullAssessment && (!r.doorKnocker && ((r.AOOtotalLowInput % 4) !== 0) )},
             msg: "Forekomstarealene må være multippel av 4_km²!"
         },
         {
             id: "(a)err13",
-            get cond() {return !r.doorKnocker && ((r.AOOtotalBestInput % 4) !== 0) },
+            get cond() {return r.doFullAssessment && (!r.doorKnocker && ((r.AOOtotalBestInput % 4) !== 0) )},
             msg: "Forekomstarealene må være multippel av 4_km²!"
         },
         {
             id: "(a)err14",
-            get cond() {return !r.doorKnocker && ((r.AOOtotalHighInput % 4) !== 0) },
+            get cond() {return r.doFullAssessment && (!r.doorKnocker && ((r.AOOtotalHighInput % 4) !== 0) )},
             msg: "Forekomstarealene må være multippel av 4_km²!"
         },
         {
             id: "(a)err15",
-            get cond() {return !r.doorKnocker && ((r.AOO50yrLowInput % 4) !== 0) },
+            get cond() {return r.doFullAssessment && (!r.doorKnocker && ((r.AOO50yrLowInput % 4) !== 0) )},
             msg: "Forekomstarealene må være multippel av 4_km²!"
         },
         {
             id: "(a)err16",
-            get cond() {return !r.doorKnocker && ((r.AOO50yrBestInput % 4) !== 0) },
+            get cond() {return r.doFullAssessment && (!r.doorKnocker && ((r.AOO50yrBestInput % 4) !== 0) )},
             msg: "Forekomstarealene må være multippel av 4_km²!"
         },
         {
             id: "(a)err17",
-            get cond() {return !r.doorKnocker && ((r.AOO50yrHighInput % 4) !== 0) },
+            get cond() {return r.doFullAssessment && (!r.doorKnocker && ((r.AOO50yrHighInput % 4) !== 0) )},
             msg: "Forekomstarealene må være multippel av 4_km²!"
         },
         {
             id: "(a)warn1",
-            get cond() {return !r.doorKnocker && (r.AOOknownInput % 4 || r.AOOtotalLowInput % 4 || r.AOOtotalBestInput % 4 || r.AOOtotalHighInput % 4 || r.AOO50yrLowInput % 4 || r.AOO50yrBestInput % 4 || r.AOO50yrHighInput % 4 || false)},
+            get cond() {return r.doFullAssessment && (!r.doorKnocker && (r.AOOknownInput % 4 || r.AOOtotalLowInput % 4 || r.AOOtotalBestInput % 4 || r.AOOtotalHighInput % 4 || r.AOO50yrLowInput % 4 || r.AOO50yrBestInput % 4 || r.AOO50yrHighInput % 4 || false))},
             type: "warning",
             msg: "MERK: Forekomstarealene vil bli avrundet oppover til nærmeste større multippel av 4_km²."
         },
         {
             id: "(a)warn2",
-            get cond() {return r.AOO1 > r.AOOknown1},
+            get cond() {return r.doFullAssessment && (r.AOO1 > r.AOOknown1)},
             type: "warning",
             msg: "MERK: Estimatet skal kun justeres om forekomster som er utgått pga. tiltak ble tatt ut av estimatet for kjent forekomstareal"
         },
         {
             id: "(a)warn3",
-            get cond() {return r.AOO2 > r.AOOknown2},
+            get cond() {return r.doFullAssessment && (r.AOO2 > r.AOOknown2)},
             type: "warning",
             msg: "MERK: Estimatet skal kun justeres om forekomster som er utgått pga. tiltak ble tatt ut av estimatet for kjent forekomstareal"
         },
 
         {
             id: "(b)err1",
-            get cond() {return r.occurrences1Low > r.occurrences1Best},
+            get cond() {return r.doFullAssessment && (r.occurrences1Low > r.occurrences1Best)},
             msg: "Det nedre anslaget på antall forekomster kan ikke være større enn det beste anslaget!"
         },
         {
             id: "(b)err2",
-            get cond() {return r.occurrences1High < r.occurrences1Best},
+            get cond() {return r.doFullAssessment && (r.occurrences1High < r.occurrences1Best)},
             msg: "Det øvre anslaget på antall forekomster kan ikke være mindre enn det beste anslaget!"
         },
         {
             id: "Aerr1",
-            get cond() {return r.ametodkey === "AmethodInvalid"},
+            get cond() {return r.doFullAssessment && (r.ametodkey === "AmethodInvalid")},
             msg: "Metode for A-kriteriet er ikke valgt."
         },
         {
