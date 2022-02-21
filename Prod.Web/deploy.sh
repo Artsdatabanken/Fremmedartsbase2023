11fda0dfbea348cf95474c7e0ddb6758b7749ec3
Myrdrahl <even.juberg@gmail.com>
2022-02-21 15:37:47 +0100

    Update deploy.sh
    Bør rette utfordringene med automatisk deploy.

--- Prod.Web/deploy.sh
+++ Prod.Web/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 echo "Removing old artifacts"
-curl -X POST -H 'Content-type: application/json' --data '{"text":"deploy rl2021-frontend"}' $slackaddy
+curl -X POST -H 'Content-type: application/json' --data '{"text":"deploy rl2021-frontend"}' $POST_SLACK
 echo "Deploying artifacts to test"
 sshpass -p $scp_pass scp -v -r -o StrictHostKeyChecking=no  ~/build/Artsdatabanken/Rodliste2019/Prod.Web/dist/* $scp_user@$scp_dest
