6f3725022c905be14cceef85686bf65be149c77d
Svetlana Naumova <svetlana.naumova@artsdatabanken.no>
2022-03-07 10:42:26 +0100

    Fix #520

--- Prod.Web/src/components/assessment/assessment52Utbredelse.js
+++ Prod.Web/src/components/assessment/assessment52Utbredelse.js
@@ -319,7 +319,7 @@ export default class Assessment52Utbredelse extends React.Component {
                                 <div className="labels distribution dk">
                                     <p>Hvor mange 2 x 2 km-ruter kan arten kolonisere i løpet av en 10 års-periode basert på én introduksjon til norsk natur? (Innenfor vurderingsperioden på 50 år)?</p>
                                     <p>Hvor mange ytterligere introduksjoner til norsk natur antas arten å få i løpet av samme 10-års periode?</p>
-                                    <p>Totalt forekomstareal <b> 10 år etter første introduksjon </b> (km<sup>2</sup>)</p>
+                                    <p style={{marginTop: "180px"}}>Totalt forekomstareal <b> 10 år etter første introduksjon </b> (km<sup>2</sup>)</p>
                                     
                                 </div>
                                 <div className="distribution dk">
