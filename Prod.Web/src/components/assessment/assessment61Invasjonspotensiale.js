2a468ebdfa4cbfd8beda24cc616d779d8ce731fa
Ane <ane.myhre@artsdatabanken.no>
2022-03-31 20:14:51 +0200

    try fix larger table B-crit and when RedList shows

--- Prod.Web/src/components/assessment/assessment61Invasjonspotensiale.js
+++ Prod.Web/src/components/assessment/assessment61Invasjonspotensiale.js
@@ -484,7 +484,8 @@ export default class Assessment61Invasjonspotensiale extends React.Component {
                     }}>{labels.AppHeader.assessmentSave}</Xcomp.Button> */}
                 </div> : 
                // riskAssessment.roAscore2018 != null &&
-                <p><b>Den utgåtte metoden c) Rødlistekriterier var brukt på A-kriteriet for denne arten i 2018. Det resulterte i skår {riskAssessment.roAscore2018 + 1} på A-kriteriet.</b></p> }
+               riskAssessment.chosenSpreadMedanLifespan == "RedListCategoryLevel" ?
+                <p><b>Den utgåtte metoden c) Rødlistekriterier var brukt på A-kriteriet for denne arten i 2018. Det resulterte i skår {riskAssessment.roAscore2018 + 1} på A-kriteriet.</b></p>: <p></p> }
 
                 {riskAssessment.chosenSpreadMedanLifespan != "RedListCategoryLevel" && riskAssessment.chosenSpreadMedanLifespan != "" && 
                     <Criterion criterion={critA} 
