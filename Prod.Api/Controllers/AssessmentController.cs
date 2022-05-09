86332405b97740956e27ab1ea601c956fb557bf8
bugdriven <helgesandmark@hotmail.com>
2022-05-09 09:19:29 +0200

    AssessmentController - removed a "fix"

--- Prod.Api/Controllers/AssessmentController.cs
+++ Prod.Api/Controllers/AssessmentController.cs
@@ -65,16 +65,6 @@ namespace Prod.Api.Controllers
             //    doc.B2BeregnetAreal = "";
             //}
 
-
-            if (doc.Category == "NK")
-            {
-                var ass2018 = doc.PreviousAssessments.FirstOrDefault(x => x.RevisionYear == 2018);
-                if(ass2018 != null && ass2018.MainCategory == "NotApplicable")
-                {
-                    doc.Category = "NR";
-                }
-            }
-
             await SendMessage("assessment", "open");
 
             // Safeguard
