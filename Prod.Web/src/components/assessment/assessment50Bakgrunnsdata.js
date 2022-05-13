e227aff45edbea7f403e5e6209127137a9a11b71
bugdriven <helgesandmark@hotmail.com>
2022-05-13 10:59:03 +0200

    cleanup 13

--- Prod.Web/src/components/assessment/assessment50Bakgrunnsdata.js
+++ Prod.Web/src/components/assessment/assessment50Bakgrunnsdata.js
@@ -1,30 +1,20 @@
 import React from 'react'
-import config from '../../config';
 import {observer, inject} from 'mobx-react'
-import * as Xcomp from './observableComponents'
 import Tabs from '../tabs'
 import Assessment51Naturtyper from './assessment51Naturtyper'
 import Assessment52Utbredelse from './assessment52Utbredelse'
 @inject('appState')
 @observer
-
 export default class Assessment50Bakgrunnsdata extends React.Component {
     render() {
-        const {appState:{assessment}, appState, appState:{infoTabs}} = this.props
-        // const {appState:{assessment}, vurdering, fabModel} = this.props
-        //const labels = fabModel.kodeLabels.DistributionHistory
-        
-        // const labels = fabModel.codeLabels.DistributionHistory
-        const labels = appState.codeLabels.DistributionHistory
+        const {appState:{infoTabs}} = this.props
         return (
             <div>
-                    <Tabs clName={"nav_menu submenu"} tabData={infoTabs}/>
-                    {
-                        infoTabs.activeTab.id === 2  ?
-                        <Assessment51Naturtyper/> :
-                        <Assessment52Utbredelse/>
-                        //<Utbredelseshistorikk vurdering={vurdering} fabModel={appState}/>
-                    }
+                <Tabs clName={"nav_menu submenu"} tabData={infoTabs}/>
+                {infoTabs.activeTab.id === 2 
+                ? <Assessment51Naturtyper/>
+                : <Assessment52Utbredelse/>
+                }
             </div>
         )
     }
