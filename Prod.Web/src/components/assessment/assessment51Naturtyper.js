1c75f8094e1e6f21c91755ca74e16e6c1c2e3286
bugdriven <helgesandmark@hotmail.com>
2022-05-13 11:14:13 +0200

    cleanup 15

--- Prod.Web/src/components/assessment/assessment51Naturtyper.js
+++ Prod.Web/src/components/assessment/assessment51Naturtyper.js
@@ -40,32 +40,32 @@ export default class Assessment51Naturtyper extends React.Component {
         const marine = (assessment.expertGroup == "Marine invertebrater" || assessment.expertGroup == "Marine invertebrater (Svalbard)" || assessment.expertGroup == "Alger" || assessment.expertGroup == "Fisker" || assessment.expertGroup == "Fisker (Svalbard)")
         const mainland = assessment.expertGroup != "Marine invertebrater" && assessment.expertGroup.indexOf ("Svalbard") < 0
         const svalbard = (assessment.expertGroup == "Karplanter (Svalbard)" || assessment.expertGroup == "Pattedyr (Svalbard)")
-        const climateZoneLabel = (id) => koder.naturalOriginClimateZone.find(code => code.Value === id).Text
+        // const climateZoneLabel = (id) => koder.naturalOriginClimateZone.find(code => code.Value === id).Text
         const contBioClimateZoneLabel = (id) => koder.continentalBioClimateZone.find(code => code.Value === id) ? koder.continentalBioClimateZone.find(code => code.Value === id).Text : id
         const arcticBioClimateZoneLabel = (id) => koder.arcticBioCLimateZone.find(code => code.Value === id) ? koder.arcticBioCLimateZone.find(code => code.Value === id).Text : id
         const coastLineZoneLabel = (id) => koder.coastLineZone.find(code => code.Value === id) ? koder.coastLineZone.find(code => code.Value === id).Text : id
-        const subClimateZoneLabel = (id) => koder.naturalOriginSubClimateZone.find(code => code.Value === id).Text
-        const naturalOriginDisabled = (id, region) => koder.naturalOriginDisabled.find(code => code.Value === id).Text.indexOf(region) !== -1
+        // const subClimateZoneLabel = (id) => koder.naturalOriginSubClimateZone.find(code => code.Value === id).Text
+        // const naturalOriginDisabled = (id, region) => koder.naturalOriginDisabled.find(code => code.Value === id).Text.indexOf(region) !== -1
         const coastZoneDisabled = (id, region) => koder.coastZoneDisabled.find(code => code.Value === id) ? koder.coastZoneDisabled.find(code => code.Value === id).Text.indexOf(region) !== -1 : true
         const arcticZoneDisabled = (id, region) => koder.arcticZoneDisabled.find(code => code.Value === id) ? koder.arcticZoneDisabled.find(code => code.Value === id).Text.indexOf(region) !== -1 : true
         const mainlandZoneDisabled = (id, region) => koder.mainlandZoneDisabled.find(code => code.Value === id) ? koder.mainlandZoneDisabled.find(code => code.Value === id).Text.indexOf(region) !== -1 : true
         // const nts = appState.naturtyper
         // const doms = appState.dominansSkog
-        var hasImpactAbroad = false 
-        if (assessment.impactedNatureTypes.length > 0) {
-            for (var i = 0; i < assessment.impactedNatureTypes.length; i++) {
-                if (assessment.impactedNatureTypes[i].background.indexOf("ObservationAbroad") > -1 || assessment.impactedNatureTypes[i].background.indexOf("WrittenDocumentationAbroad") > -1){
-                    hasImpactAbroad = true;
-                }
-            }
-        }
+        // var hasImpactAbroad = false 
+        // if (assessment.impactedNatureTypes.length > 0) {
+        //     for (var i = 0; i < assessment.impactedNatureTypes.length; i++) {
+        //         if (assessment.impactedNatureTypes[i].background.indexOf("ObservationAbroad") > -1 || assessment.impactedNatureTypes[i].background.indexOf("WrittenDocumentationAbroad") > -1){
+        //             hasImpactAbroad = true;
+        //         }
+        //     }
+        // }
         const canRenderTable = !!appState.naturtypeLabels
         return (
             <div>
                 <fieldset className="well"> 
-                <h2>{ntLabels.heading}</h2>   
-                <div>
-                    {appState.trueteogsjeldneCodes
+                    <h2>{ntLabels.heading}</h2>   
+                    <div>
+                        {appState.trueteogsjeldneCodes
                         ? <NewNaturetype
                             mode="truet"
                             assessment={assessment}
@@ -78,50 +78,55 @@ export default class Assessment51Naturtyper extends React.Component {
                             superheader={ntLabels.redListEffects} >
                         </NewNaturetype>
                         : null}
-                </div>
-                <NewNaturetype
-                    mode="nin"
-                    appState={appState}
-                    assessment={assessment}
-                    addNaturtype={this.addNaturtype}
-                    labels={labels}
-                    codes={appState.naturtyperNIN2}
-                    header={ntLabels.chooseNT}
-                    superheader={ntLabels.effectsNiN23} >
-                </NewNaturetype>
-                {assessment.impactedNatureTypes.length > 0 && 
-                <>
-                <h4>{ntLabels.chosenNatureTypes}</h4>
-                <NaturtypeTable
-                    naturetypes={assessment.impactedNatureTypes}
-                    appState={appState}
-                    canRenderTable={canRenderTable}
-                    labels={labels}
-                    codes={koder}
-                    disabled={disabled}
-                    desc={ntLabels.colonizedAreaDescription}/>
-                </>
-                }
-                {assessment.impactedNatureTypesFrom2018.length > 0 && <div className="previousAssessment">
-                <h4>{ntLabels.dataFromPreviousAssessment}</h4>
-                <NaturtypeTable
-                    naturetypes={assessment.impactedNatureTypesFrom2018}
-                    appState={appState}
-                    canRenderTable={canRenderTable}
-                    labels={labels}
-                    codes={koder}
-                    disabled={true}
-                    desc={ntLabels.transferredFromNiN22}/>
-                </div>}
-                {assessment.redlistedNatureTypes.length > 0 && <div className="previousAssessment">
-                {assessment.impactedNatureTypesFrom2018.length == 0 && <h4>{ntLabels.dataFromPreviousAssessment}</h4>}
-                <p>{ntLabels.redlistedNaturetypes2011}</p>
-                <RedlistedNaturetypeTable
-                    naturetypes={assessment.redlistedNatureTypes}
-                    canRenderTable={canRenderTable}
-                    labels={labels}
-                    fabModel={appState}/>
-                </div>}
+                    </div>
+                    <NewNaturetype
+                        mode="nin"
+                        appState={appState}
+                        assessment={assessment}
+                        addNaturtype={this.addNaturtype}
+                        labels={labels}
+                        codes={appState.naturtyperNIN2}
+                        header={ntLabels.chooseNT}
+                        superheader={ntLabels.effectsNiN23} >
+                    </NewNaturetype>
+                    {assessment.impactedNatureTypes.length > 0
+                    ? <>
+                    <h4>{ntLabels.chosenNatureTypes}</h4>
+                    <NaturtypeTable
+                        naturetypes={assessment.impactedNatureTypes}
+                        appState={appState}
+                        canRenderTable={canRenderTable}
+                        labels={labels}
+                        codes={koder}
+                        disabled={disabled}
+                        desc={ntLabels.colonizedAreaDescription}/>
+                    </>
+                    : null}
+                    {assessment.impactedNatureTypesFrom2018.length > 0 
+                    ? <div className="previousAssessment">
+                        <h4>{ntLabels.dataFromPreviousAssessment}</h4>
+                        <NaturtypeTable
+                            naturetypes={assessment.impactedNatureTypesFrom2018}
+                            appState={appState}
+                            canRenderTable={canRenderTable}
+                            labels={labels}
+                            codes={koder}
+                            disabled={true}
+                            desc={ntLabels.transferredFromNiN22}/>
+                    </div>
+                    : null}
+                    {assessment.redlistedNatureTypes.length > 0 
+                    ? <div className="previousAssessment">
+                        {assessment.impactedNatureTypesFrom2018.length == 0 
+                        && <h4>{ntLabels.dataFromPreviousAssessment}</h4>}
+                        <p>{ntLabels.redlistedNaturetypes2011}</p>
+                        <RedlistedNaturetypeTable
+                        naturetypes={assessment.redlistedNatureTypes}
+                        canRenderTable={canRenderTable}
+                        labels={labels}
+                        fabModel={appState}/>
+                    </div>
+                    : null}
                 </fieldset> 
                 <fieldset className="well">
                     <h4>{ntLabels.critCHeading}</h4>
@@ -133,74 +138,72 @@ export default class Assessment51Naturtyper extends React.Component {
                     <p>{ntLabels.criteriumF}</p>
                     <Criterion criterion={critF} mode="noheading" disabled={disabled}/>
                     <p>{ntLabels.natureAffected}</p>
-                    <Xcomp.HtmlString 
-                        observableValue={[riskAssessment, 'threatenedNatureTypesAffectedDomesticDescription']} />
+                    <Xcomp.HtmlString observableValue={[riskAssessment, 'threatenedNatureTypesAffectedDomesticDescription']} />
                 </fieldset>
                 <fieldset className="well">
                     <h4>{ntLabels.critGHeading}</h4>
                     <p>{ntLabels.criteriumG}</p>
                     <Criterion criterion={critG} mode="noheading" disabled={disabled}/>
                     <p>{ntLabels.natureAffected}</p>
-                    <Xcomp.HtmlString
-                        observableValue={[riskAssessment, 'commonNatureTypesAffectedDomesticDescription']}/>
+                    <Xcomp.HtmlString observableValue={[riskAssessment, 'commonNatureTypesAffectedDomesticDescription']}/>
                 </fieldset>
                 <fieldset className="well">
-                <h4>{ntLabels.regionalNaturetypes}</h4>
-                <p> Mer informasjon og kartvisning av soner og seksjoner finner du <a href="https://nin.artsdatabanken.no/Natur_i_Norge/Natursystem/Beskrivelsessystem/Regional_naturvariasjon?informasjon" target="_blank"> her</a>. </p>
-                <br></br>
-                {marine && 
-                <>
-                    <h5 className={"bioCLZone"}>{ntLabels.marineSpecies}</h5>
-                    <p className="bioCLZone">{ntLabels.coastWaterSections}</p>
-                    <div className="bioclimaticZones">
-                    <div>{ntLabels.coastWaterZones}</div>
-                    <OriginTable 
-                            mode = {"marine"}
-                            origins={assessment.coastLineSections} 
-                            climateZoneLabel={coastLineZoneLabel}
-                            naturalOriginDisabled={coastZoneDisabled}
-                            labels={labels.NaturalOrigin}
-                    />
-                    </div>
-                </>
-                }
-                {mainland && 
-                <>
-                    <h5 className={"bioCLZone"}>{ntLabels.mainlandNorway}</h5>
-                    <p className="bioCLZone">{ntLabels.bioClimateSections}</p>
-                    <div className="bioclimaticZones">
-                    <div>{ntLabels.bioClimateZones}</div>
-                    <OriginTable 
-                            mode = {"continental"}
-                            origins={assessment.currentBioClimateZones} 
-                            climateZoneLabel={contBioClimateZoneLabel}
-                            naturalOriginDisabled={mainlandZoneDisabled}
-                            labels={labels.NaturalOrigin}
-                    />
-                    </div>
-                </>
-                }
-                {svalbard && 
-                <>                   
-                    <h5 className={"bioCLZone"}>{ntLabels.svalbard}</h5>
-                    <p className="bioCLZone">{ntLabels.bioClimateSections}</p>
-                    <div className="bioclimaticZones">
-                    <div>{ntLabels.bioClimateZonesArctic}</div>
-                    <OriginTable 
-                            mode = {"arctic"}
-                            origins={assessment.arcticBioClimateZones} 
-                            climateZoneLabel={arcticBioClimateZoneLabel}
-                            naturalOriginDisabled={arcticZoneDisabled}
-                            labels={labels.NaturalOrigin}
-                    />
-                    </div>
-                </>
-                }
+                    <h4>{ntLabels.regionalNaturetypes}</h4>
+                    <p> Mer informasjon og kartvisning av soner og seksjoner finner du <a href="https://nin.artsdatabanken.no/Natur_i_Norge/Natursystem/Beskrivelsessystem/Regional_naturvariasjon?informasjon" target="_blank"> her</a>. </p>
+                    <br/>
+                    {marine 
+                    ? <>
+                        <h5 className={"bioCLZone"}>{ntLabels.marineSpecies}</h5>
+                        <p className="bioCLZone">{ntLabels.coastWaterSections}</p>
+                        <div className="bioclimaticZones">
+                        <div>{ntLabels.coastWaterZones}</div>
+                        <OriginTable 
+                                mode = {"marine"}
+                                origins={assessment.coastLineSections} 
+                                climateZoneLabel={coastLineZoneLabel}
+                                naturalOriginDisabled={coastZoneDisabled}
+                                labels={labels.NaturalOrigin}
+                        />
+                        </div>
+                    </>
+                    : null}
+                    {mainland 
+                    ? <>
+                        <h5 className={"bioCLZone"}>{ntLabels.mainlandNorway}</h5>
+                        <p className="bioCLZone">{ntLabels.bioClimateSections}</p>
+                        <div className="bioclimaticZones">
+                        <div>{ntLabels.bioClimateZones}</div>
+                        <OriginTable 
+                                mode = {"continental"}
+                                origins={assessment.currentBioClimateZones} 
+                                climateZoneLabel={contBioClimateZoneLabel}
+                                naturalOriginDisabled={mainlandZoneDisabled}
+                                labels={labels.NaturalOrigin}
+                        />
+                        </div>
+                    </>
+                    : null}
+                    {svalbard 
+                    ? <>                   
+                        <h5 className={"bioCLZone"}>{ntLabels.svalbard}</h5>
+                        <p className="bioCLZone">{ntLabels.bioClimateSections}</p>
+                        <div className="bioclimaticZones">
+                        <div>{ntLabels.bioClimateZonesArctic}</div>
+                        <OriginTable 
+                                mode = {"arctic"}
+                                origins={assessment.arcticBioClimateZones} 
+                                climateZoneLabel={arcticBioClimateZoneLabel}
+                                naturalOriginDisabled={arcticZoneDisabled}
+                                labels={labels.NaturalOrigin}
+                        />
+                        </div>
+                    </>
+                    : null}
                 </fieldset>
-            {true || appState.livsmediumEnabled
+                {true || appState.livsmediumEnabled
                 ? <fieldset className="well">
                     {appState.livsmediumCodes
-                        ? <NewNaturetype
+                    ? <NewNaturetype
                         mode="livsmedium"
                         assessment={assessment}
                         appState={appState}
@@ -210,17 +213,16 @@ export default class Assessment51Naturtyper extends React.Component {
                         header={labels.NatureTypes.chooseLM}
                         hideStateChange={true}
                         superheader={"Livsmedium"}
-                         >
-                        </NewNaturetype>
-                        : null}
-                {assessment.habitats.length > 0 && 
-                    <HabitatTable
-                    canRenderTable={canRenderTable}
-                    naturetypes={assessment.habitats}
-                    labels={labels}
-                    fabModel={appState}
                     />
-                }
+                    : null}
+                    {assessment.habitats.length > 0
+                    ? <HabitatTable
+                        canRenderTable={canRenderTable}
+                        naturetypes={assessment.habitats}
+                        labels={labels}
+                        fabModel={appState}
+                        />
+                    : null}
                 </fieldset>
                 : null}
                 <br/>
