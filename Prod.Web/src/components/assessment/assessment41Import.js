84c65b2da436f49cdf8d05b74371f76701835eb9
bugdriven <helgesandmark@hotmail.com>
2022-05-16 11:11:57 +0200

    cleanup 21

--- Prod.Web/src/components/assessment/assessment41Import.js
+++ Prod.Web/src/components/assessment/assessment41Import.js
@@ -27,21 +27,21 @@ export default class Assessment41Import extends React.Component {
         }
     }
 
-    @action removeImportPathway = (vurdering, value) => {
-        const result = vurdering.importPathways.remove(value);
-        // console.log("item removed : " + result)
-    };
+    // @action removeImportPathway = (vurdering, value) => {
+    //     const result = vurdering.importPathways.remove(value);
+    //     // console.log("item removed : " + result)
+    // };
 
     render() {
         const {appState:{assessment:{riskAssessment}}, appState:{assessment}, appState} = this.props;
-        const vurdering = assessment
+        // const vurdering = assessment
         const labels = appState.codeLabels
-        const koder = appState.koder
+        // const koder = appState.koder
         const disabled = appState.userContext.readonly
         // const importPathways = vurdering.importPathways
         // const importPathwayKoder = appState.spredningsveier.children.filter(child => child.name === "Import")
         const nbsp = "\u00a0"
-        const removeImportPathway = (mp) => this.removeImportPathway(vurdering, mp)
+        // const removeImportPathway = (mp) => this.removeImportPathway(vurdering, mp)
 
         return(
         <div>
@@ -51,28 +51,26 @@ export default class Assessment41Import extends React.Component {
                 <div>
                 <p style={{marginLeft: '20px'}}>Spres arten utelukkende direkte til norsk natur (uten å gå veien om innendørsareal eller artens eget produksjonsareal)?</p>
                 <Xcomp.Radio
-                            label={labels.indoorProduktionImport.a}
-                            value={"positive"}
-                            //defaultChecked = {assessment.importedToIndoorOrProductionArea}
-                            disabled = {disabled}
-                            observableValue={[assessment, "indoorProduktion"]}/>
+                    label={labels.indoorProduktionImport.a}
+                    value={"positive"}
+                    disabled = {disabled}
+                    observableValue={[assessment, "indoorProduktion"]}/>
                 <Xcomp.Radio
-                            label={labels.indoorProduktionImport.b}
-                            value={"negative"}
-                            disabled = {disabled}
-                            observableValue={[assessment, "indoorProduktion"]}/>
+                    label={labels.indoorProduktionImport.b}
+                    value={"negative"}
+                    disabled = {disabled}
+                    observableValue={[assessment, "indoorProduktion"]}/>
                 </div>
                 </fieldset>
                     {assessment.indoorProduktion != null &&
                     <div>
                         {assessment.indoorProduktion == "negative" 
-                                ? <>  
-                                    <Assessment42Spredningsveier  name={"Til innendørs- eller produksjonsareal"} furtherInfo={labels.Import.furtherInfoIndoors}/>  
-                                  </>
-                                : null
-                        }
-                            <Assessment42Spredningsveier name={"Introduksjon til natur"} furtherInfo={labels.Import.furtherInfoIntro}/>
-                            <Assessment42Spredningsveier name={"Videre spredning i natur"} furtherInfo={labels.Import.furtherInfoNature}/>
+                        ? <>  
+                            <Assessment42Spredningsveier  name={"Til innendørs- eller produksjonsareal"} furtherInfo={labels.Import.furtherInfoIndoors}/>  
+                            </>
+                        : null}
+                        <Assessment42Spredningsveier name={"Introduksjon til natur"} furtherInfo={labels.Import.furtherInfoIntro}/>
+                        <Assessment42Spredningsveier name={"Videre spredning i natur"} furtherInfo={labels.Import.furtherInfoNature}/>
                      </div>
                     }
                 {config.showPageHeaders ? <h3>{labels.Import.importIndoor}</h3> : <br />}
