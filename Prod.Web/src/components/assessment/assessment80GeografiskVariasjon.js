be2068107fdb43e07510947f340a6c42949d0082
bugdriven <helgesandmark@hotmail.com>
2022-05-18 09:57:03 +0200

    cleanup 31

--- Prod.Web/src/components/assessment/assessment80GeografiskVariasjon.js
+++ Prod.Web/src/components/assessment/assessment80GeografiskVariasjon.js
@@ -14,39 +14,51 @@ export default class Assessment80GeografiskVariasjon extends React.Component {
 		const geolabels = labels.geographicVariation
 		return(
 			<div>
-				{config.showPageHeaders ? <h3>{geolabels.heading}</h3> :  <br />}
+				{config.showPageHeaders 
+				? <h3>{geolabels.heading}</h3> 
+				: <br />}
 				<fieldset className="well">
-				<h2>{geolabels.heading}</h2>
-					{riskAssessment.riskLevelCode == "NK" ? 
-						<p dangerouslySetInnerHTML={{ __html: labels.geographicVariation.possibleLowerCategoryNK }}></p> :
-						riskAssessment.riskLevelCode == "SE" || riskAssessment.riskLevelCode == "HI"  ?
-						<p dangerouslySetInnerHTML={{ __html: labels.geographicVariation.possibleLowerCategorySEHI }}></p> :
-						riskAssessment.riskLevelCode == "PH"  ?
-						<p dangerouslySetInnerHTML={{ __html: labels.geographicVariation.possibleLowerCategoryPH }}></p> :
-						riskAssessment.riskLevelCode == "LO"  ?
-						<p dangerouslySetInnerHTML={{ __html: labels.geographicVariation.possibleLowerCategoryLO }}></p> :
-						null}
-					<Xcomp.StringEnum observableValue={[riskAssessment, "possibleLowerCategory"]} disabled={riskAssessment.riskLevelCode == "NK" 
-																										} mode="radio" codes={koder.yesNo}/>
-					{(riskAssessment.riskLevelCode == "NK" 
-						) &&
-						<p>{labels.geographicVariation.notValid }</p> }
-					
-					{riskAssessment.possibleLowerCategory == "yes" ? 
-					<><p>{labels.geographicVariation.reasonForGeographicalVariation}</p>
-						{assessment.marine ? <Xcomp.MultiselectArray className="geoVar" observableValue={[riskAssessment, "geographicalVariation"]} mode="check" 
+					<h2>{geolabels.heading}</h2>
+					{riskAssessment.riskLevelCode === "NK" 
+					? <p dangerouslySetInnerHTML={{ __html: labels.geographicVariation.possibleLowerCategoryNK }}></p>
+					: riskAssessment.riskLevelCode === "SE" || riskAssessment.riskLevelCode == "HI" 
+					? <p dangerouslySetInnerHTML={{ __html: labels.geographicVariation.possibleLowerCategorySEHI }}></p>
+					: riskAssessment.riskLevelCode === "PH"
+					? <p dangerouslySetInnerHTML={{ __html: labels.geographicVariation.possibleLowerCategoryPH }}></p>
+					: riskAssessment.riskLevelCode === "LO"
+					? <p dangerouslySetInnerHTML={{ __html: labels.geographicVariation.possibleLowerCategoryLO }}></p>
+					: null}
+					<Xcomp.StringEnum 
+						observableValue={[riskAssessment, "possibleLowerCategory"]} 
+						disabled={riskAssessment.riskLevelCode === "NK"} 
+						mode="radio" 
+						codes={koder.yesNo}/>
+					{(riskAssessment.riskLevelCode == "NK") 
+					? <p>{labels.geographicVariation.notValid}</p> 
+					: null}
+					{riskAssessment.possibleLowerCategory == "yes"
+					? <>
+					<p>{labels.geographicVariation.reasonForGeographicalVariation}</p>
+					{assessment.marine 
+					? <Xcomp.MultiselectArray 
+						className="geoVar" 
+						observableValue={[riskAssessment, "geographicalVariation"]} 
+						mode="check" 
 						codes={koder.geographicalVariationMarine} 
-					/> :
-					<Xcomp.MultiselectArray className="geoVar" observableValue={[riskAssessment, "geographicalVariation"]} mode="check" 
-						codes={koder.geographicalVariation} 
 					/>
-					}
-					{riskAssessment.geographicalVariation.length > 0 ?
-					<div className="geoVarInput">
-					<Xcomp.HtmlString  observableValue={[riskAssessment, "geographicalVariationDocumentation"]} /> 
-					</div>:
-					null}</> : null
-					}
+					: <Xcomp.MultiselectArray 
+						className="geoVar" 
+						observableValue={[riskAssessment, "geographicalVariation"]} 
+						mode="check" 
+						codes={koder.geographicalVariation} 
+					/>}
+					{riskAssessment.geographicalVariation.length > 0
+					? <div className="geoVarInput">
+						<Xcomp.HtmlString  observableValue={[riskAssessment, "geographicalVariationDocumentation"]} /> 
+					</div>
+					: null}
+					</> 
+					: null}
 				</fieldset>
 			</div>
         );
