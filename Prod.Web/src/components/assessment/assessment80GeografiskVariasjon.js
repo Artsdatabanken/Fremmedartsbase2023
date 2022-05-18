0fc99f3ed02dc70de11a43de1ea01e4d1a6906ca
bugdriven <helgesandmark@hotmail.com>
2022-05-18 09:50:16 +0200

    cleanup 30

--- Prod.Web/src/components/assessment/assessment80GeografiskVariasjon.js
+++ Prod.Web/src/components/assessment/assessment80GeografiskVariasjon.js
@@ -4,8 +4,6 @@ import PropTypes from 'prop-types'
 import {observer, inject} from 'mobx-react';
 import {observable, autorun} from 'mobx';
 import * as Xcomp from './observableComponents';
-
-
 @inject("appState")
 @observer
 export default class Assessment80GeografiskVariasjon extends React.Component {
@@ -14,7 +12,6 @@ export default class Assessment80GeografiskVariasjon extends React.Component {
         const labels = appState.codeLabels
 		const koder = appState.koder
 		const geolabels = labels.geographicVariation
-		
 		return(
 			<div>
 				{config.showPageHeaders ? <h3>{geolabels.heading}</h3> :  <br />}
@@ -30,10 +27,8 @@ export default class Assessment80GeografiskVariasjon extends React.Component {
 						<p dangerouslySetInnerHTML={{ __html: labels.geographicVariation.possibleLowerCategoryLO }}></p> :
 						null}
 					<Xcomp.StringEnum observableValue={[riskAssessment, "possibleLowerCategory"]} disabled={riskAssessment.riskLevelCode == "NK" 
-																										//|| riskAssessment.riskLevelCode == "LO"
 																										} mode="radio" codes={koder.yesNo}/>
 					{(riskAssessment.riskLevelCode == "NK" 
-						//|| riskAssessment.riskLevelCode == "LO"
 						) &&
 						<p>{labels.geographicVariation.notValid }</p> }
 					
@@ -42,27 +37,18 @@ export default class Assessment80GeografiskVariasjon extends React.Component {
 						{assessment.marine ? <Xcomp.MultiselectArray className="geoVar" observableValue={[riskAssessment, "geographicalVariation"]} mode="check" 
 						codes={koder.geographicalVariationMarine} 
 					/> :
-
 					<Xcomp.MultiselectArray className="geoVar" observableValue={[riskAssessment, "geographicalVariation"]} mode="check" 
 						codes={koder.geographicalVariation} 
 					/>
-				
 					}
 					{riskAssessment.geographicalVariation.length > 0 ?
 					<div className="geoVarInput">
-					{/*<label>{geolabels.extendedInformation}</label>*/}
 					<Xcomp.HtmlString  observableValue={[riskAssessment, "geographicalVariationDocumentation"]} /> 
 					</div>:
 					null}</> : null
 					}
-					
 				</fieldset>
 			</div>
         );
 	}
 }
-
-// Vurdering53GeografiskVariasjon.propTypes = {
-// 	viewModel: PropTypes.object.isRequired,
-// 	riskAssessment: PropTypes.object.isRequired
-// }
