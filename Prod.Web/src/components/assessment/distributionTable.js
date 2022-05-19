ad20a14936fcdd57f74f3765374c99407f1a61ac
bugdriven <helgesandmark@hotmail.com>
2022-05-19 10:51:43 +0200

    cleanup 39

--- Prod.Web/src/components/assessment/distributionTable.js
+++ Prod.Web/src/components/assessment/distributionTable.js
@@ -27,22 +27,20 @@ export default class DistributionTable extends React.Component {
             <col style={{width: "33%"}} /> 
             <col style={{width: "34%"}} />
         </colgroup>}
-        {!assessment.isDoorKnocker ?
-            <thead>
-                <tr>
-                    <th>Lavt anslag (25-prosentil)</th>
-                    <th>Beste anslag (median) </th>
-                    <th> Høyt anslag (75-prosentil)</th>
-                    <th>&nbsp;</th>
-                </tr>
-            </thead> : 
-            <thead>
-            <tr>
+        <thead>
+            {!assessment.isDoorKnocker
+            ? <tr>
                 <th>Lavt anslag (25-prosentil)</th>
                 <th>Beste anslag (median) </th>
                 <th> Høyt anslag (75-prosentil)</th>
+                <th>&nbsp;</th>
             </tr>
-        </thead> }
+            : <tr>
+                <th>Lavt anslag (25-prosentil)</th>
+                <th>Beste anslag (median) </th>
+                <th> Høyt anslag (75-prosentil)</th>
+            </tr>}
+        </thead>
         <tbody>            
             <tr>
                 <td>
@@ -59,103 +57,102 @@ export default class DistributionTable extends React.Component {
                     disabled={disabled}
                     integer
                    />   
-
                 </td>
-               <td>
-               <Xcomp.Number                            
-                    observableValue={assessment.isDoorKnocker ? [assessment.riskAssessment, "occurrences1High"] :[assessment.riskAssessment, "AOOtotalHighInput"]}
-                    observableErrors={assessment.isDoorKnocker ? [errorhandler, "(b)err2"] : [errorhandler, "(a)err3", "(a)err14" ]}
-                    disabled={disabled}
-                    integer />   
+                <td>
+                    <Xcomp.Number                            
+                        observableValue={assessment.isDoorKnocker ? [assessment.riskAssessment, "occurrences1High"] :[assessment.riskAssessment, "AOOtotalHighInput"]}
+                        observableErrors={assessment.isDoorKnocker ? [errorhandler, "(b)err2"] : [errorhandler, "(a)err3", "(a)err14" ]}
+                        disabled={disabled}
+                        integer />   
                 </td>
-                {!assessment.isDoorKnocker &&
-                    <td>km<sup>2</sup></td>
-                }
-                           
+                {!assessment.isDoorKnocker
+                ? <td>km<sup>2</sup></td>
+                : null}
             </tr>
             {/* Styling of the table for door knockers because of large amount of text to the left*/}
             <tr style={assessment.isDoorKnocker ? {marginTop: "30px" } : {marginTop: "5px"}}>
                 <td>
-                <Xcomp.Number                            
-                    observableValue={assessment.isDoorKnocker ? [assessment.riskAssessment, "introductionsLow"] : [assessment.riskAssessment, "AOOdarkfigureLow"]}
-                    disabled={disabled}
-                    integer />   
+                    <Xcomp.Number                            
+                        observableValue={assessment.isDoorKnocker ? [assessment.riskAssessment, "introductionsLow"] : [assessment.riskAssessment, "AOOdarkfigureLow"]}
+                        disabled={disabled}
+                        integer />   
                 </td>                
                 <td>
-                <Xcomp.Number                            
-                    observableValue={assessment.isDoorKnocker ? [assessment.riskAssessment, "introductionsBest"] : [assessment.riskAssessment, "AOOdarkfigureBest"]}
-                    disabled={disabled}
-                    integer />   
+                    <Xcomp.Number                            
+                        observableValue={assessment.isDoorKnocker ? [assessment.riskAssessment, "introductionsBest"] : [assessment.riskAssessment, "AOOdarkfigureBest"]}
+                        disabled={disabled}
+                        integer />   
                 </td>
-               <td>
-               <Xcomp.Number                            
-                    observableValue={assessment.isDoorKnocker ? [assessment.riskAssessment, "introductionsHigh"] : [assessment.riskAssessment, "AOOdarkfigureHigh"]}
-                    disabled={disabled}
-                    integer />   
-              </td>
-              {!assessment.isDoorKnocker &&
-                    <td>mørketall</td>
-             }
+                <td>
+                    <Xcomp.Number                            
+                        observableValue={assessment.isDoorKnocker ? [assessment.riskAssessment, "introductionsHigh"] : [assessment.riskAssessment, "AOOdarkfigureHigh"]}
+                        disabled={disabled}
+                        integer />   
+                </td>
+                {!assessment.isDoorKnocker
+                ? <td>mørketall</td>
+                : null}
             </tr>           
         </tbody>
     </table>
     
-     {assessment.isDoorKnocker && <h5>Antatt</h5> }
+    {assessment.isDoorKnocker
+    ? <h5>Antatt</h5> 
+    : null}
     <table className="table distribution">
-    {!assessment.isDoorKnocker ?
-        <colgroup>
+        {!assessment.isDoorKnocker
+        ? <colgroup>
             <col style={{width: "33%"}} />
             <col style={{width: "33%"}} /> 
             <col style={{width: "17%"}} />
             <col style={{width: "17%"}} />
-        </colgroup>: 
-        <colgroup>
+        </colgroup>
+        : <colgroup>
             <col style={{width: "33%"}} />
             <col style={{width: "33%"}} /> 
             <col style={{width: "34%"}} />
         </colgroup>}
-    <thead>
-        <tr>
-            <th>Lavt anslag (25-prosentil)</th>
-            <th>Beste anslag (median) </th>
-            <th> Høyt anslag (75-prosentil)</th>
-            {!assessment.isDoorKnocker && 
-                    <th>&nbsp;</th>
-            }
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td>
-            <Xcomp.Number                            
-                observableValue={assessment.isDoorKnocker ? [assessment.riskAssessment, "AOO10yrLow"] : [assessment.riskAssessment, "AOO50yrLowInput"]}
-                observableErrors={assessment.isDoorKnocker ? [errorhandler, "B1err1"] : [errorhandler, "(a)err4", "(a)err15"]}
-                disabled={disabled || assessment.isDoorKnocker}
-                integer 
-                />   
-            </td>            
-            <td>
-            <Xcomp.Number                            
-                observableValue={assessment.isDoorKnocker ? [assessment.riskAssessment, "AOO10yrBest"] : [assessment.riskAssessment, "AOO50yrBestInput"]}
-                observableErrors={assessment.isDoorKnocker ? [errorhandler, "B1err1", "B1err2"] : [errorhandler, "(a)err4", "(a)err5", "(a)err16"]}
-                disabled={disabled || assessment.isDoorKnocker}
-                integer 
-                />   
-            </td>
-           <td>
-           <Xcomp.Number                            
-                observableValue={assessment.isDoorKnocker ? [assessment.riskAssessment, "AOO10yrHigh"] :[assessment.riskAssessment, "AOO50yrHighInput"]}
-                observableErrors={assessment.isDoorKnocker ? [errorhandler, "B1err2"] : [errorhandler, "(a)err5", "(a)err17"]}
-                disabled={disabled || assessment.isDoorKnocker}
-                integer 
-                />   
-          </td>
-          {!assessment.isDoorKnocker && 
-            <td>km<sup>2</sup></td>
-          }
-        </tr>  
-    </tbody>
-</table></>)
+        <thead>
+            <tr>
+                <th>Lavt anslag (25-prosentil)</th>
+                <th>Beste anslag (median) </th>
+                <th> Høyt anslag (75-prosentil)</th>
+                {!assessment.isDoorKnocker
+                ? <th>&nbsp;</th>
+                : null}
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>
+                    <Xcomp.Number                            
+                        observableValue={assessment.isDoorKnocker ? [assessment.riskAssessment, "AOO10yrLow"] : [assessment.riskAssessment, "AOO50yrLowInput"]}
+                        observableErrors={assessment.isDoorKnocker ? [errorhandler, "B1err1"] : [errorhandler, "(a)err4", "(a)err15"]}
+                        disabled={disabled || assessment.isDoorKnocker}
+                        integer 
+                        />   
+                </td>            
+                <td>
+                    <Xcomp.Number                            
+                        observableValue={assessment.isDoorKnocker ? [assessment.riskAssessment, "AOO10yrBest"] : [assessment.riskAssessment, "AOO50yrBestInput"]}
+                        observableErrors={assessment.isDoorKnocker ? [errorhandler, "B1err1", "B1err2"] : [errorhandler, "(a)err4", "(a)err5", "(a)err16"]}
+                        disabled={disabled || assessment.isDoorKnocker}
+                        integer 
+                        />   
+                </td>
+                <td>
+                    <Xcomp.Number                            
+                        observableValue={assessment.isDoorKnocker ? [assessment.riskAssessment, "AOO10yrHigh"] :[assessment.riskAssessment, "AOO50yrHighInput"]}
+                        observableErrors={assessment.isDoorKnocker ? [errorhandler, "B1err2"] : [errorhandler, "(a)err5", "(a)err17"]}
+                        disabled={disabled || assessment.isDoorKnocker}
+                        integer 
+                        />   
+                </td>
+                {!assessment.isDoorKnocker
+                ? <td>km<sup>2</sup></td>
+                : null}
+            </tr>  
+        </tbody>
+    </table></>)
     }
 }
-
