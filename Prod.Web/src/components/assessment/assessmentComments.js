6314cd33eeff223c9c2dd70d6a8ac6a96c182ad7
Stein Arild Hoem <stein.hoem@artsdatabanken.no>
2022-06-03 13:46:15 +0200

    fix #619 Fix rettighet til å lukke kommentarer

--- Prod.Web/src/components/assessment/assessmentComments.js
+++ Prod.Web/src/components/assessment/assessmentComments.js
@@ -224,7 +224,7 @@ export default inject('appState')(observer(class AssessmentComments extends Comp
                                             <path fillRule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
                                         </svg>
                                         </button>                                   
-                                        { (rolle.admin || rolle.skriver) && 
+                                        { (rolle.admin || rolle.writeAccess) && 
                                         <button className="btn btn-primary btn-xs" disabled={item.closed}  onClick={() => {this.closeComment(item.id, appState)}}>{labels.processed}</button> } 
                                         </td> 
                                         : <td> </td>}
