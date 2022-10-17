87da083e6886d1a6f9ae347eca1c3f1802eedf8c
steinho <stein.hoem@gmail.com>
2022-10-17 13:18:23 +0200

    Test fix #677

--- Prod.Domain/Helpers/JsonHelpers.cs
+++ Prod.Domain/Helpers/JsonHelpers.cs
@@ -70,6 +70,41 @@ namespace Prod.Domain.Helpers
                 }
             }
         }
+        public class NotNullableStringJsonConverter : JsonConverter<string>
+        {
+            public override string Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+            {
+                if (reader.TokenType == JsonTokenType.Null)
+                {
+                    return string.Empty;
+                }
+                if (reader.TokenType == JsonTokenType.Number)
+                {
+                    var int32 = reader.GetInt32();
+                    if (int32 == 0)
+                    {
+                        return string.Empty;
+                    }
+                    return int32.ToString();
+                }
+
+                return reader.GetString();
+            }
+
+            public override void Write(Utf8JsonWriter writer, string value, JsonSerializerOptions options)
+            {
+                // For performance, lift up the writer implementation.
+                if (value == null)
+                {
+                    writer.WriteStringValue(string.Empty);
+                    writer.WriteNullValue();
+                }
+                else
+                {
+                    writer.WriteStringValue(value.AsSpan());
+                }
+            }
+        }
 
 
         public class BoolJsonConverter : JsonConverter<bool>
