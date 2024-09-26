829779b81d3c38ecacb9c9315202832774cf1572
vebjorna-artsdatabanken <168717707+vebjorna-artsdatabanken@users.noreply.github.com>
2024-09-26 10:41:22 +0200

    show appView when isServicesReady is true

--- Prod.Web/src/app.jsx
+++ Prod.Web/src/app.jsx
@@ -1,28 +1,28 @@
 import "./style/cleanstyle.less"
 import React from 'react';
-import {inject, observer} from 'mobx-react'
+import { inject, observer } from 'mobx-react'
 
 import * as Xcomp from './components/observableComponents'
-// import Assessment from './components/assessment'
-// import SelectAssessment from './components/selectAssessment'
+import Loading from './components/Loading'
 import AppView from './components/appView'
 
-const mainView = ({appState, appState:{assessment, isServicesReady}, appState:{router}}) => (
+const mainView = ({ appState, appState: { assessment, isServicesReady }, appState: { router } }) => (
   <div className="page_padding">
     <div className="header_background">
-        <div className="header_padding" style={{display: 'flex'}}>
-          <img src="//artsdatabanken.no/Files/7809" className="top_image" crossOrigin="anonymous"/>
-{(assessment && assessment.evaluatedVernacularName) ? <h1 className="header_title" style={{width: '90%'}}>Fremmede arter 2023{assessment && ": "+ assessment.evaluatedScientificName + ", " + assessment.evaluatedVernacularName +". Kategori: " + assessment.category}</h1>  : 
-<h1 className="header_title" style={{width: '90%'}}>Fremmede arter 2023{assessment && ": "+ assessment.evaluatedScientificName + ". Kategori: " + assessment.category}</h1>}
+      <div className="header_padding" style={{ display: 'flex' }}>
+        <img src="//artsdatabanken.no/Files/7809" className="top_image" crossOrigin="anonymous" />
+        {(assessment && assessment.evaluatedVernacularName) ? <h1 className="header_title" style={{ width: '90%' }}>Fremmede arter 2023{assessment && ": " + assessment.evaluatedScientificName + ", " + assessment.evaluatedVernacularName + ". Kategori: " + assessment.category}</h1> :
+          <h1 className="header_title" style={{ width: '90%' }}>Fremmede arter 2023{assessment && ": " + assessment.evaluatedScientificName + ". Kategori: " + assessment.category}</h1>}
 
-           {assessment && assessment.evaluationStatus !== 'finished' &&      <Xcomp.Button primary onClick= {() => {
-                    console.log("Save assessment")
-                    appState.saveCurrentAssessment();
-                  }}>Lagre</Xcomp.Button> }
+        {assessment && assessment.evaluationStatus !== 'finished' && <Xcomp.Button primary onClick={() => {
+          console.log("Save assessment")
+          appState.saveCurrentAssessment();
+        }}>Lagre</Xcomp.Button>}
       </div>
     </div>
     {/* {isServicesReady ? <SelectAssessment /> : null }
     {(assessment && isServicesReady) ? <Assessment /> : null } */}
-    <AppView isServicesReady={isServicesReady}/>
+    {isServicesReady ? <AppView/> : <Loading/>}
   </div>)
-  export default inject('appState')(observer(mainView));
+
+export default inject('appState')(observer(mainView));
