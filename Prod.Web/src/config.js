e19644d398364e2a19ce538dee8e1b3012ba4bf0
vebjorna-artsdatabanken <168717707+vebjorna-artsdatabanken@users.noreply.github.com>
2024-10-14 16:46:31 +0200

    Rework config.js + env files

--- Prod.Web/src/config.js
+++ Prod.Web/src/config.js
@@ -1,33 +1,3 @@
-function apiUrl(anchor) {
-    //const relPath = '/lr/' + GIT_BRANCH + '_api/'
-    switch (anchor) {
-        case '#lokalapi':
-            return 'http://localhost:25808'
-        case '#remoteapi':
-            return 'https://fab4api.test.artsdatabanken.no/'
-        default:
-            //return relPath   // This does not currently work because api is not relative to ("under") app
-            if ((window.location.href).indexOf("test.") > -1) { return 'https://fab4api.test.artsdatabanken.no' }
-            // return 'https://fab4api.test.artsdatabanken.no'
-            return import.meta.env.DEV ? 'http://localhost:25808' : 'https://fab4.artsdatabanken.no'
-    }
-}
-function referenceApiUrl(anchor) {
-    //return relPath   // This does not currently work because api is not relative to ("under") app
-    if ((window.location.href).indexOf("test.") > -1) { return 'https://referenceapi.test.artsdatabanken.no/' }
-    return import.meta.env.DEV ? 'https://referenceapi.test.artsdatabanken.no/' : 'https://referenceapi.artsdatabanken.no/'
-}
-
-function mapApiUrl(anchor) {
-    const relPath = '/supportapi/listhelper/'
-    switch (anchor) {
-        case '#lokalapi':
-            return 'http://localhost:58772/listhelper/'
-        case '#pavlov':
-        default:
-            return 'http://it-webadbtest01.it.ntnu.no' + relPath
-    }
-}
 
 function overlayMapUrl(anchor) {
     const relPath = '/supportapi/MetriaCache/metriawmsproxy?LAYERS=MetriaTatortPlus'
@@ -49,18 +19,12 @@ function isNotTest() {
 }
 
 const homeuri = window.location.protocol + "//" + window.location.host
-// const homeuri =
-//   (process.env.NODE_ENV === 'development') ?
-//   "http://localhost:12345" :
-//   (window.location.href.indexOf("test.") > 1) ?
-//   "https://fab4api.test.artsdatabanken.no" :
-//   "https://fab4api.test.artsdatabanken.no"
 
 
 
 const authconfig =
 {
-    authority: "https://id.artsdatabanken.no/",
+    authority: import.meta.env.VITE_AUTHORITY_URL,
     client_id: "fab4",
     redirect_uri: homeuri + "/#signin-oidc",
     silent_redirect_uri: homeuri + "/#silentredirect",
@@ -76,69 +40,16 @@ const authconfig =
     response_mode: "query"
 }
 
-
-//   const authconfig =
-//   (process.env.NODE_ENV === 'development') ?
-//   {
-//       authority: "https://id.artsdatabanken.no/",
-//       client_id: "fab4",
-//       redirect_uri: "http://localhost:1234/#signin-oidc",
-//       silent_redirect_uri: "http://localhost:1234/#silentredirect",
-//       popup_redirect_uri: "http://localhost:1234/#silentredirect",
-//       post_logout_redirect_uri: "http://localhost:1234/#post-logout",
-//       response_type: "code",
-//       scope: "openid profile roles email fab4api",
-//       automaticSilentRenew: true,
-//       acr_values: "Level3",
-//       ui_locales: "nb",
-//       loadUserInfo: true,
-//       revokeAccessTokenOnSignout: true,
-//       response_mode: "query"
-//   } : (window.location.href.indexOf("test.") > 1 ?
-//   {
-//       authority: "https://id.artsdatabanken.no/",
-//       client_id: "fab4",
-//       redirect_uri: "https://fab4api.test.artsdatabanken.no/#signin-oidc",
-//       silent_redirect_uri: "https://fab4api.test.artsdatabanken.no/#silentredirect",
-//       popup_redirect_uri: "https://fab4api.test.artsdatabanken.no/#silentredirect",
-//       post_logout_redirect_uri: "https://fab4api.test.artsdatabanken.no/#post-logout",
-//       response_type: "code",
-//       scope: "openid profile roles email fab4api",
-//       automaticSilentRenew: true,
-//       acr_values: "Level3",
-//       ui_locales: "nb",
-//       loadUserInfo: true,
-//       revokeAccessTokenOnSignout: true,
-//       response_mode: "query"
-//   }:
-//   {
-//     authority: "https://id.artsdatabanken.no/",
-//     client_id: "fab4",
-//     redirect_uri: "https://fab4api.test.artsdatabanken.no/#signin-oidc",
-//     silent_redirect_uri: "https://fab4api.test.artsdatabanken.no/#silentredirect",
-//     popup_redirect_uri: "https://fab4api.test.artsdatabanken.no/#silentredirect",
-//     post_logout_redirect_uri: "https://fab4api.test.artsdatabanken.no/#post-logout",
-//     response_type: "code",
-//     scope: "openid profile roles email fab4api",
-//     automaticSilentRenew: true,
-//     acr_values: "Level3",
-//     ui_locales: "nb",
-//     loadUserInfo: true,
-//     revokeAccessTokenOnSignout: true,
-//     response_mode: "query"
-// });
-
-
 const config = {
     // apiUrl: "https://rl2019api.artsdatabanken.no",   // "http://localhost:25807",
-    apiUrl: apiUrl(window.location.hash),
+    apiUrl: import.meta.env.VITE_BACKEND_URL,
     getUrl: resource => config.apiUrl + "/api/" + resource,
-    getSignalRUrl: apiUrl(window.location.hash) + "/messageHub",
-    mapApiUrl: mapApiUrl(window.location.hash),
+    // getSignalRUrl: apiUrl(window.location.hash) + "/messageHub",
+    mapApiUrl: import.meta.env.VITE_ARTSKART_BASEURL,
     mapEpsgCode: 32633,
     mapEpsgDef: "+proj=utm +zone=33 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs",
     overlayMapUrl: overlayMapUrl(window.location.hash),
-    referenceApiUrl: referenceApiUrl(window.location.hash),
+    referenceApiUrl: import.meta.env.VITE_REFERENCE_URL,
     authconfig: authconfig,
     isRelease: isRelease(),
     isNotTest: isNotTest()
